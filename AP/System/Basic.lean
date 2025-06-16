7d9b9cf38c129c47739dba1e8062d31fa8df6447
Hakerh400 <hakerh403@gmail.com>
2025-06-16 17:09:32 +0200

    commit

--- AP/System/Basic.lean
+++ AP/System/Basic.lean
@@ -4,17 +4,17 @@ noncomputable section
 open scoped Classical
 
 @[simp]
-theorem mk_system_s₀ {S T : Type} {s₀ : S} {f : SystemFn S T} :
-(mk_system s₀ f).s₀ = s₀ := rfl
+theorem mk_system_s₀ {S T : Type} {f : SystemFn S T} {s₀ : S} :
+(mk_system f s₀).s₀ = s₀ := rfl
 
 @[simp]
-theorem mk_system_f {S T : Type} {s₀ : S} {f : SystemFn S T} :
-(mk_system s₀ f).f = mk_system_fn s₀ f := rfl
+theorem mk_system_f {S T : Type} {f : SystemFn S T} {s₀ : S} :
+(mk_system f s₀).f = mk_system_fn f s₀ := rfl
 
 @[simp]
-theorem mk_system_fn_eq_some_iff {S T : Type} {s₀ : S} {f : SystemFn S T} {s t s'} :
-mk_system_fn s₀ f s t = some s' ↔
-Reachable s₀ f s ∧ Reachable s₀ f s' ∧ f s t = some s' := by
+theorem mk_system_fn_eq_some_iff {S T : Type} {f : SystemFn S T} {s₀ : S} {s t s₁} :
+mk_system_fn f s₀ s t = some s₁ ↔
+Reachable f s₀ s ∧ Reachable f s₀ s₁ ∧ f s t = some s₁ := by
   unfold mk_system_fn guard
   split_ifs with h₁ <;> simp [h₁]
   by_cases h₂ : f s t = none; simp [h₂]
@@ -27,41 +27,41 @@ Reachable s₀ f s ∧ Reachable s₀ f s' ∧ f s t = some s' := by
   contradiction
 
 @[simp]
-theorem mk_system_fn_eq_none_iff {S T : Type} {s₀ : S} {f : SystemFn S T} {s t} :
-mk_system_fn s₀ f s t = none ↔ ¬Reachable s₀ f s ∨ f s t = none := by
+theorem mk_system_fn_eq_none_iff {S T : Type} {f : SystemFn S T} {s₀ : S} {s t} :
+mk_system_fn f s₀ s t = none ↔ ¬Reachable f s₀ s ∨ f s t = none := by
   simp [Option.eq_none_iff_forall_ne_some]
   by_cases h₁ : f s t = none; simp [h₁]
   obtain ⟨s₁, h₂⟩ := Option.exists_eq_some_of_ne_none h₁
   simp [h₂]
-  by_cases h₃ : Reachable s₀ f s <;> simp [h₃]
+  by_cases h₃ : Reachable f s₀ s <;> simp [h₃]
   exact Reachable.h₁ s t s₁ h₃ h₂
 
 @[simp]
-theorem reachable_mk_system_fn_iff {S T : Type} {s₀ : S} {f : SystemFn S T} {s} :
-Reachable s₀ (mk_system_fn s₀ f) s ↔ Reachable s₀ f s := by
+theorem reachable_mk_system_fn_iff {S T : Type} {f : SystemFn S T} {s₀ : S} {s} :
+Reachable (mk_system_fn f s₀) s₀ s ↔ Reachable f s₀ s := by
   constructor <;> intro h
   · induction h
     · exact Reachable.h₀
     clear s
-    nm s t s' h₁ h₂ ih
+    nm s t s₁ h₁ h₂ ih
     simp at h₂
     exact h₂.2.1
   · induction h
     · exact Reachable.h₀
     clear s
-    nm s t s' h₁ h₂ ih
-    apply Reachable.h₁ s t s' ih
+    nm s t s₁ h₁ h₂ ih
+    apply Reachable.h₁ s t s₁ ih
     simp [h₁, h₂]
-    exact Reachable.h₁ s t s' h₁ h₂
+    exact Reachable.h₁ s t s₁ h₁ h₂
 
 @[simp]
-theorem mk_system_fn_mk_system_fn {S T : Type} {s₀ : S} {f : SystemFn S T} :
-mk_system_fn s₀ (mk_system_fn s₀ f) = mk_system_fn s₀ f := by
-  ext s t s'; simp; tauto
+theorem mk_system_fn_mk_system_fn {S T : Type} {f : SystemFn S T} {s₀ : S} :
+mk_system_fn (mk_system_fn f s₀) s₀ = mk_system_fn f s₀ := by
+  ext s t s₁; simp; tauto
 
 @[simp]
-theorem mk_system_mk_system_fn {S T : Type} {s₀ : S} {f : SystemFn S T} :
-mk_system s₀ (mk_system_fn s₀ f) = mk_system s₀ f := by
+theorem mk_system_mk_system_fn {S T : Type} {f : SystemFn S T} {s₀ : S} :
+mk_system (mk_system_fn f s₀) s₀ = mk_system f s₀ := by
   ext1; rfl; simp
 
 @[simp]
@@ -71,3 +71,25 @@ sys.init_state.toSystem = sys := rfl
 @[simp]
 theorem system_init_state_state {S T : Type} {sys : System S T} :
 sys.init_state.s = sys.s₀ := rfl
+
+@[simp, refl]
+theorem Reachable.refl {S T : Type} {f : SystemFn S T} {a} :
+Reachable f a a := Reachable.h₀
+
+@[simp, trans]
+theorem Reachable.trans {S T : Type} {f : SystemFn S T} {a b c}
+(hab : Reachable f a b) (hbc : Reachable f b c) : Reachable f a c := by
+  induction hbc; exact hab; apply Reachable.h₁ <;> assumption
+
+theorem system_state_state_mk_system_tr_eq {S T : Type}
+{st : SystemState S T} {f s₀ t s₁}
+(h₁ : st.toSystem = mk_system f s₀)
+(h₂ : st.tr t = some s₁) :
+f st.s t = some s₁.s := by
+  unfold SystemState.tr at h₂
+  split at h₂ <;> simp at h₂
+  nm s₂ h₃
+  subst h₂
+  generalize_proofs
+  simp [h₁] at h₃
+  convert h₃.2.2
--- AP/System/Defs.lean
+++ AP/System/Defs.lean
@@ -3,12 +3,12 @@ import AP.Util
 abbrev SystemFn (S T : Type) : Type :=
   S → T → Option S
 
-inductive Reachable {S T : Type} (s₀ : S) (f : SystemFn S T) : S → Prop where
-| h₀ : Reachable s₀ f s₀
-| h₁ : ∀ (s t s'), Reachable s₀ f s → f s t = some s' → Reachable s₀ f s'
+inductive Reachable {S T : Type} (f : SystemFn S T) (s₀ : S) : S → Prop where
+| h₀ : Reachable f s₀ s₀
+| h₁ : ∀ (s t s₁), Reachable f s₀ s → f s t = some s₁ → Reachable f s₀ s₁
 
-def system_cnd {S T : Type} (s₀ : S) (f : SystemFn S T) : Prop :=
-  ∀ s, ¬Reachable s₀ f s →
+def system_cnd {S T : Type} (f : SystemFn S T) (s₀ : S) : Prop :=
+  ∀ s, ¬Reachable f s₀ s →
   (∀ t, f s t = none) ∧
   (∀ s₁ t, f s₁ t ≠ some s)
 
@@ -16,19 +16,19 @@ def system_cnd {S T : Type} (s₀ : S) (f : SystemFn S T) : Prop :=
 structure System (S T : Type) : Type where
   s₀ : S
   f : SystemFn S T
-  h_sys : system_cnd s₀ f
+  h_sys : system_cnd f s₀
 
-def mk_system_fn {S T : Type} (s₀ : S) (f : SystemFn S T)
-[∀ s, Decidable # Reachable s₀ f s] : SystemFn S T :=
+def mk_system_fn {S T : Type} (f : SystemFn S T) (s₀ : S)
+[∀ s, Decidable # Reachable f s₀ s] : SystemFn S T :=
   λ s t => do
-  guard # Reachable s₀ f s
-  let s' ← f s t
-  guard # Reachable s₀ f s'
-  return s'
+  guard # Reachable f s₀ s
+  let s₁ ← f s t
+  guard # Reachable f s₀ s₁
+  return s₁
 
 @[simp]
-theorem system_cnd_mk_system_fn {S T : Type} {s₀} {f : SystemFn S T}
-[∀ s, Decidable # Reachable s₀ f s] : system_cnd s₀ (mk_system_fn s₀ f) := by
+theorem system_cnd_mk_system_fn {S T : Type} {f : SystemFn S T} {s₀}
+[∀ s, Decidable # Reachable f s₀ s] : system_cnd (mk_system_fn f s₀) s₀ := by
   unfold mk_system_fn
   intro s hs
   constructor
@@ -40,23 +40,23 @@ theorem system_cnd_mk_system_fn {S T : Type} {s₀} {f : SystemFn S T}
     induction h₁
     · exact Reachable.h₀
     clear s t
-    nm s t s' h₁ h₂ ih
-    apply Reachable.h₁ s t s' ih
+    nm s t s₁ h₁ h₂ ih
+    apply Reachable.h₁ s t s₁ ih
     have h₃ := Reachable.h₁ _ _ _ h₁ h₂
     simp [h₁, h₂, h₃]
   · intro s₁ t
     contrapose! hs
-    have h₁ : Reachable s₀ f s₁ :=
+    have h₁ : Reachable f s₀ s₁ :=
       by
         by_contra h₁
         simp [h₁] at hs
-    obtain ⟨s', h₂⟩ : ∃ s', f s₁ t = some s' :=
+    obtain ⟨s₁, h₂⟩ : ∃ s₂, f s₁ t = some s₂ :=
       by
         by_contra! h₂
         rw [←Option.eq_none_iff_forall_ne_some] at h₂
         simp [h₂] at hs
     simp [h₁, h₂] at hs
-    have h₃ : Reachable s₀ f s' :=
+    have h₃ : Reachable f s₀ s₁ :=
       by
         by_contra h₃
         simp [h₃] at hs
@@ -66,24 +66,29 @@ theorem system_cnd_mk_system_fn {S T : Type} {s₀} {f : SystemFn S T}
     induction h₃
     · exact Reachable.h₀
     clear s
-    nm s t s' h₁ h₂ ih
-    apply Reachable.h₁ s t s' ih
+    nm s t s₁ h₁ h₂ ih
+    apply Reachable.h₁ s t s₁ ih
     have h₃ := Reachable.h₁ _ _ _ h₁ h₂
     simp [h₁, h₂, h₃]
 
-def mk_system {S T : Type} (s₀ : S) (f : SystemFn S T)
-[∀ s, Decidable # Reachable s₀ f s] : System S T :=
+def mk_system {S T : Type} (f : SystemFn S T) (s₀ : S)
+[∀ s, Decidable # Reachable f s₀ s] : System S T :=
   { s₀ := s₀
-  , f := mk_system_fn s₀ f
+  , f := mk_system_fn f s₀
   , h_sys := system_cnd_mk_system_fn
   }
 
+@[simp]
 def System.Reachable {S T : Type} (sys : System S T) (s : S) : Prop :=
-  _root_.Reachable sys.s₀ sys.f s
+  _root_.Reachable sys.f sys.s₀ s
 
 structure SystemState (S T : Type) extends System S T where
   s : S
   h_st : toSystem.Reachable s
+
+@[simp]
+def SystemState.Reachable {S T : Type} (st : SystemState S T) (s : S) : Prop :=
+  _root_.Reachable st.f st.s s
 
 def System.init_state {S T : Type} (sys : System S T) : SystemState S T :=
   { toSystem := sys
@@ -91,7 +96,7 @@ def System.init_state {S T : Type} (sys : System S T) : SystemState S T :=
   , h_st := Reachable.h₀
   }
 
-def SystemState.move {S T : Type} (st : SystemState S T) (t : T) :
+def SystemState.tr {S T : Type} (st : SystemState S T) (t : T) :
 Option (SystemState S T) := match h : st.f st.s t with
 | none => none
 | some s₁ => some
@@ -99,3 +104,10 @@ Option (SystemState S T) := match h : st.f st.s t with
     s := s₁
   , h_st := Reachable.h₁ st.s t s₁ st.h_st h
   }
+
+def SystemState.trs {S T : Type} (st : SystemState S T) (ts : List T) :
+Option (SystemState S T) := match ts with
+| [] => st
+| (t :: ts) => do
+  let st ← st.tr t
+  st.trs ts
