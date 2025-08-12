2a2b8f6f27b0f08066c045dc6a013022fd32b9c7
Hakerh400 <hakerh403@gmail.com>
2025-08-12 16:27:47 +0200

    commit

--- AP/Util/Data/Trie.lean
+++ AP/Util/Data/Trie.lean
@@ -4,40 +4,29 @@ namespace Trie
 
 open Std
 
-inductive Raw (α β : Type*) [DecidableEq α] [Hashable α] where
-| mk : Option β → DHashMap.Raw α (λ _ => Raw α β) → Raw α β
-deriving Repr
-
 variable {α β : Type*} [ha₁ : DecidableEq α] [ha₂ : Hashable α]
 
-def Raw.val : Raw α β → Option β
+inductive Raw₀ (α β : Type*) [DecidableEq α] [Hashable α] where
+| mk : Option β → (mp : DHashMap.Raw α (λ _ => Raw₀ α β)) → Raw₀ α β
+
+namespace Raw₀
+
+def val : Raw₀ α β → Option β
 | .mk val _ => val
 
-def Raw.mp : Raw α β → DHashMap.Raw α (λ _ => Raw α β)
+def mp : Raw₀ α β → DHashMap.Raw α (λ _ => Raw₀ α β)
 | .mk _ mp => mp
 
 @[simp]
-theorem Raw.val_mk {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw α β)} :
+theorem val_mk {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)} :
 (mk val mp).val = val := rfl
 
 @[simp]
-theorem Raw.mp_mk {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw α β)} :
+theorem mp_mk {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)} :
 (mk val mp).mp = mp := rfl
 
--- private noncomputable
--- def Raw.toSet_aux (t : Raw α β) : Set (Raw α β) :=
---   {t' | ∃ k, t.mp.get? k = some t'}
--- 
--- private noncomputable
--- def Raw.toSet_bind_aux (s : Set (Raw α β)) : Set (Raw α β) :=
---   ⋃ t ∈ s, t.toSet_aux
--- 
--- private
--- theorem exi_depth_aux {t : Raw α β} : ∃ n, Raw.toSet_bind_aux^[n] {t} = ∅ := by
---   have := @t.recOn
-
 noncomputable
-def Raw.depthAux (t : Raw α β) : ℕ :=
+def depthAux (t : Raw₀ α β) : ℕ :=
   let f := @t.recOn
   @f (λ _ => ℕ) (λ _ => ℕ) (λ _ => ℕ) (λ _ => ℕ) (λ _ => ℕ)
     (λ _ _ n => n)
@@ -45,14 +34,6 @@ def Raw.depthAux (t : Raw α β) : ℕ :=
     (λ _ n => n)
     0 (λ _ _ n m => max n m)
     0 (λ _ _ _ n m => max (n + 1) m)
-
--- #check 0 #exit
-
--- theorem aux₁ {α : Type*} {β : α → Type*} [ha₁ : DecidableEq α] [ha₂ : Hashable α] :
--- ((DHashMap.Raw.ofList (α := α) (β := β)) []).2.toList = [] := by
---   simp
-
--- #check 0 #exit
 
 -- example : Raw.depthAux (α := ℕ) (β := ℕ) (Raw.mk none (.ofList [])) = 0 := by
 --   simp only [Raw.depthAux, DHashMap.Raw.ofList_nil]
@@ -75,25 +56,50 @@ def Raw.depthAux (t : Raw α β) : ℕ :=
 --   nm k t b
 --   simp at h₁
 
-theorem Raw.depthAux_le {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw α β)}
-{k : α} {t : Raw α β} (h : mp.get? k = t) : t.depthAux < (Raw.mk val mp).depthAux := by
+-- #check 0 #exit
+
+theorem depthAux_le {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)}
+{k : α} {t : Raw₀ α β} (h : mp.get? k = t) : t.depthAux < (mk val mp).depthAux := by
   sorry
 
 -- #check 0 #exit
 
 set_option linter.unusedVariables false in
-def Raw.rec' {γ : Raw α β → Sort*}
-(motive : ∀ (val : Option β) (mp : DHashMap.Raw α (λ _ => Raw α β)),
-(∀ i x, mp.get? i = some x → γ x) → γ (mk val mp)) : (t : Raw α β) → γ t
+def rec' {γ : Raw₀ α β → Sort*}
+(motive : ∀ (val : Option β) (mp : DHashMap.Raw α (λ _ => Raw₀ α β)),
+(∀ i t, mp.get? i = some t → γ t) → γ (mk val mp)) : (t : Raw₀ α β) → γ t
 | .mk val mp => motive val mp # λ i t h => t.rec' motive
 termination_by t => t.depthAux
 decreasing_by exact depthAux_le h
 
--- #check 0 #exit
+inductive WF : Raw₀ α β → Prop where
+| mk : ∀ {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)},
+  (∀ {k t}, mp.get? k = some t → t.WF) → (mk val mp).WF
 
-def Raw.depth : Raw α β → ℕ :=
-  Raw.rec' # λ val mp f => (mp.foldlWith sorry · 0) # by
-  intro acc i x h
-  exact max acc # 1 + f i x sorry
+end Raw₀
+
+structure Raw (α β : Type*) [DecidableEq α] [Hashable α] where
+  inner : Raw₀ α β
+  wf : inner.WF
+
+namespace Raw
+
+set_option linter.unusedVariables false in
+def rec' {γ : Raw α β → Sort*}
+(motive : ∀ (val : Option β) (mp : DHashMap.Raw α (λ _ => Raw₀ α β)) wf,
+(∀ i t, mp.get? i = some t → (wf : t.WF) → γ ⟨t, wf⟩) → γ ⟨Raw₀.mk val mp, wf⟩) :
+(t : Raw α β) → γ t
+| .mk (Raw₀.mk val mp) wf => motive val mp wf # λ i t h₁ wf₁ => (mk t wf₁).rec' motive
+termination_by t => t.1.depthAux
+decreasing_by exact Raw₀.depthAux_le h₁
+
+#check 0 #exit
+
+-- def Raw.depth : Raw α β → ℕ :=
+--   Raw.rec' # λ val mp f => (mp.foldlWith sorry · 0) # by
+--   intro acc i x h
+--   exact max acc # 1 + f i x sorry
+
+end Raw
 
 end Trie
