92af93c9c3b76d4e8c7e65b39bdc30d0402e5f6c
Hakerh400 <hakerh403@gmail.com>
2025-08-12 16:27:56 +0200

    commit

--- AP/Util/Data/Trie.lean
+++ AP/Util/Data/Trie.lean
@@ -25,6 +25,16 @@ theorem val_mk {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)} :
 theorem mp_mk {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)} :
 (mk val mp).mp = mp := rfl
 
+inductive WF : Raw₀ α β → Prop where
+| mk : ∀ {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)}, mp.WF →
+  (∀ {k t}, mp.get? k = some t → t.WF) → (mk val mp).WF
+
+theorem WF.mp {t : Raw₀ α β} (wf : t.WF) : t.mp.WF := by
+  cases wf; assumption
+
+theorem WF.get? {t : Raw₀ α β} (wf : t.WF) {k t'} (h : t.mp.get? k = some t') : t'.WF := by
+  cases wf; nm val mp h₁ h₂; exact h₂ h
+
 noncomputable
 def depthAux (t : Raw₀ α β) : ℕ :=
   let f := @t.recOn
@@ -35,46 +45,42 @@ def depthAux (t : Raw₀ α β) : ℕ :=
     0 (λ _ _ n m => max n m)
     0 (λ _ _ _ n m => max (n + 1) m)
 
--- example : Raw.depthAux (α := ℕ) (β := ℕ) (Raw.mk none (.ofList [])) = 0 := by
---   simp only [Raw.depthAux, DHashMap.Raw.ofList_nil]
---   generalize hx : (∅ : DHashMap.Raw _ _).2.toList = xs
---   replace hx : ∀ b ∈ xs, b.toList = []
---   · intro b hb
---     rw [List.eq_nil_iff_forall_not_mem]
---     rintro ⟨k, t⟩
---     subst hx
---     simp at hb
---     sorry
---   induction xs; rfl
---   nm b bs ih
---   simp only [Nat.max_eq_zero_iff]
---   simp at hx
---   rcases hx with ⟨h₁, h₂⟩
---   specialize ih h₂
---   simp [ih]
---   cases b; rfl
---   nm k t b
---   simp at h₁
-
 -- #check 0 #exit
 
-theorem depthAux_le {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)}
-{k : α} {t : Raw₀ α β} (h : mp.get? k = t) : t.depthAux < (mk val mp).depthAux := by
+theorem depthAux_le {t : Raw₀ α β} (wf : t.WF) {k t'}
+(h : t.mp.get? k = some t') : t'.depthAux < t.depthAux := by
+  have h₁ : ⟨k, t'⟩ ∈ t.mp.toList
+  · change ⟨k, t'⟩ ∈ (DHashMap.mk t.mp wf.mp).toList
+    simp [DHashMap.get?]
+    rw [DHashMap.Raw.get?] at h
+    split_ifs at h
+    exact h
+  rw [DHashMap.Internal.toList_eq_flat_buckets] at h₁
+  simp at h₁
+  obtain ⟨b, h₁, h₂⟩ := h₁
+  rcases t with ⟨val, mp⟩
+  nth_rw 2 [depthAux]
+  dsimp at h₁ h ⊢
+  replace h₁ : b ∈ mp.buckets.toList := by simpa
+  generalize hb : mp.buckets.toList = bs at h₁ ⊢
+  clear wf
+  induction bs generalizing mp
+  · simp at h₁
+  nm b₁ bs ih
+  specialize ih ⟨mp.size, ⟨b :: bs⟩⟩
   sorry
 
--- #check 0 #exit
+theorem depthAux_le_mk {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)}
+(wf : (mk val mp).WF) {k : α} {t : Raw₀ α β} (h : mp.get? k = some t) :
+t.depthAux < (mk val mp).depthAux := depthAux_le wf h
 
 set_option linter.unusedVariables false in
 def rec' {γ : Raw₀ α β → Sort*}
 (motive : ∀ (val : Option β) (mp : DHashMap.Raw α (λ _ => Raw₀ α β)),
-(∀ i t, mp.get? i = some t → γ t) → γ (mk val mp)) : (t : Raw₀ α β) → γ t
-| .mk val mp => motive val mp # λ i t h => t.rec' motive
+(∀ i t, mp.get? i = some t → γ t) → γ (mk val mp)) : (t : Raw₀ α β) → t.WF → γ t
+| .mk val mp, wf => motive val mp # λ i t h => t.rec' motive # wf.get? h
 termination_by t => t.depthAux
-decreasing_by exact depthAux_le h
-
-inductive WF : Raw₀ α β → Prop where
-| mk : ∀ {val : Option β} {mp : DHashMap.Raw α (λ _ => Raw₀ α β)},
-  (∀ {k t}, mp.get? k = some t → t.WF) → (mk val mp).WF
+decreasing_by exact depthAux_le wf h
 
 end Raw₀
 
@@ -91,9 +97,7 @@ def rec' {γ : Raw α β → Sort*}
 (t : Raw α β) → γ t
 | .mk (Raw₀.mk val mp) wf => motive val mp wf # λ i t h₁ wf₁ => (mk t wf₁).rec' motive
 termination_by t => t.1.depthAux
-decreasing_by exact Raw₀.depthAux_le h₁
-
-#check 0 #exit
+decreasing_by exact Raw₀.depthAux_le wf h₁
 
 -- def Raw.depth : Raw α β → ℕ :=
 --   Raw.rec' # λ val mp f => (mp.foldlWith sorry · 0) # by
