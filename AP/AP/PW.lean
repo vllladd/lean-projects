befb61685072be47f782674ed5d07af753c6f651
Hakerh400 <hakerh403@gmail.com>
2025-09-05 18:28:57 +0200

    commit

--- AP/AP/PW.lean
+++ AP/AP/PW.lean
@@ -49,7 +49,7 @@ theorem DState.validTr_setPw {s p pw} [hs : DState s] :
 sys.validTr (s.setPw pw) p = sys.validTr s p := by
   simp [System.validTr]
 
-theorem State.a_hws_of_setPw_le {s pw} [hs : sys.WF s]
+theorem State.a_hws_setPw_of_le {s pw} [hs : sys.WF s]
 (h₁ : s.pw ≤ pw) (h₂ : s.a_hws) : (s.setPw pw).a_hws := by
   have hs' := wf_setPw_of_le h₁
   obtain ⟨a, Ha, h₂⟩ := h₂
@@ -77,22 +77,31 @@ theorem State.a_hws_of_setPw_le {s pw} [hs : sys.WF s]
     rwa [setPw_eq_self_of]
     simp [pw_eq_of_tr h₃, ←h₄]
 
-theorem State.d_hws_of_setPw_le' {s : State} {pw} [hs : sys.WF s]
+theorem State.d_hws_of_setPw_le {s : State} {pw} [hs : sys.WF s]
 (h₁ : s.pw ≤ pw) (h₂ : (s.setPw pw).d_hws) : s.d_hws := by
   have hs' := wf_setPw_of_le h₁
   contrapose h₂; simp at h₂ ⊢
-  exact a_hws_of_setPw_le h₁ h₂
+  exact a_hws_setPw_of_le h₁ h₂
 
-theorem State.d_hws_of_setPw_le {s : State} {pw} [hs : sys.WF # s.setPw pw]
+theorem State.d_hws_setPw_of_le {s : State} {pw} [hs : sys.WF # s.setPw pw]
 (h₁ : pw ≤ s.pw) (h₂ : s.d_hws) : (s.setPw pw).d_hws := by
   generalize h₃ : s.setPw pw = s' at hs
   have h₄ : s'.pw = pw; simp [←h₃]; subst h₄
   rw [setPw_eq_comm] at h₃
   have h₄ : sys.WF s; rw [←h₃]; exact wf_setPw_of_le h₁
-  rw [←h₃] at h₂; exact d_hws_of_setPw_le' h₁ h₂
+  rw [←h₃] at h₂; exact d_hws_of_setPw_le h₁ h₂
+
+theorem State.a_hws_of_setPw_le {s : State} {pw} [hs : sys.WF # s.setPw pw]
+(h₁ : pw ≤ s.pw) (h₂ : (s.setPw pw).a_hws) : s.a_hws := by
+  generalize h₃ : s.setPw pw = s' at hs
+  have h₄ : s'.pw = pw; simp [←h₃]; subst h₄
+  rw [setPw_eq_comm] at h₃
+  have h₄ : sys.WF s; rw [←h₃]; exact wf_setPw_of_le h₁
+  rw [←h₃] at h₂; contrapose h₂; simp at h₂ ⊢
+  apply d_hws_of_setPw_le h₁; simpa [h₃]
 
 theorem a_hws_pw_of_le {pw pw'} (h₁ : pw ≤ pw') (h₂ : a_hws_pw pw) : a_hws_pw pw' :=
-  State.a_hws_of_setPw_le h₁ h₂
+  State.a_hws_setPw_of_le h₁ h₂
 
 theorem d_hws_pw_of_le {pw pw'} (h₁ : pw' ≤ pw) (h₂ : d_hws_pw pw) : d_hws_pw pw' :=
   State.d_hws_of_setPw_le h₁ h₂
