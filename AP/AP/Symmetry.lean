e5a79e3e69d20e02a84a47356fafc13de582dfa8
Hakerh400 <hakerh403@gmail.com>
2025-09-11 17:07:11 +0200

    commit

--- AP/AP/Symmetry.lean
+++ AP/AP/Symmetry.lean
@@ -1,3 +1,4 @@
 import AP.AP.Symmetry.Defs
 import AP.AP.Symmetry.Basic
 import AP.AP.Symmetry.Translation
+import AP.AP.Symmetry.Rotation
---
+++ AP/AP/Symmetry/Rotation.lean
@@ -0,0 +1,150 @@
+import AP.AP.Symmetry.Translation
+
+namespace AP
+
+def rotRight : sys.Symmetry := mkSym
+  { toFun := λ p => ⟨-p.y, p.x⟩
+  , invFun := λ p => ⟨p.y, -p.x⟩
+  , left_inv := λ _ => by simp
+  , right_inv := λ _ => by simp
+  }
+
+theorem rotRight.cnd_initial_fs_iff {s} :
+sys.Initial (rotRight.fs s) ↔ sys.Initial s := by
+  simp [rotRight, aPos₀_mkSymFsAux_eq_ite]
+  split_ifs with h
+  · simp [State.aPos₀, h]
+    have h₁ : initState s.pw none.iget ≠ s
+    · intro h₁
+      rw [←h₁] at h
+      simp at h
+    simp [h₁]
+    apply ne_of_congr (·.hist.length)
+    simp [h]
+  simp [State.ext_iff]
+  constructor
+  · rintro ⟨h₁, ⟨h₂, h₅⟩, h₃, h₄⟩
+    symm at h₄
+    simp at h₄
+    simp [h₁, h₃]
+    obtain ⟨p, h₄, h₆, h₇⟩ := h₄
+    rw [Point.ext_iff]
+    simp [h₂, h₅, h₄]
+    rw [Point.ext_iff]
+    simp [h₆, h₇]
+  · rintro ⟨h₁, h₂, h₃, h₄⟩
+    simp [h₁, h₂, h₃]
+    symm; simp
+    use s.aPos₀
+    simp [←h₂, h₄]
+
+theorem rotRight.cnd_tr_eq {s p} : sys.tr s p =
+(sys.tr (rotRight.fs s) (rotRight.ft p)).map rotRight.fs' := by
+  ext s'
+  simp [rotRight, sys, State.move]
+  split_ifs with ht
+  · simp [State.aMove]; simp [State.ext_iff]
+    intro h₁ h₂ h₃ h₄ h₅
+    constructor
+    · rintro ⟨H₁, H₂, H₃⟩
+      refine' ⟨_, _, _⟩
+      · intro H₄
+        contrapose! H₁
+        ext <;> assumption
+      · intro p₁ hp₁ hp₂
+        contrapose! H₂
+        convert hp₁
+        ext <;> simp_all
+      · simp [Point.dist] at H₃ ⊢
+        rw [add_comm]
+        rw [Int.add_neg_eq_sub]
+        rw [abs_sub_comm]
+        exact H₃.symm
+    · rintro ⟨H₁, H₂, H₃⟩
+      refine' ⟨_, _, _⟩
+      · rintro rfl
+        simp at H₁
+      · intro H₄
+        specialize H₂ _ H₄
+        simp at H₂
+      · simp [Point.dist] at H₃ ⊢
+        rw [add_comm] at H₃
+        rw [Int.add_neg_eq_sub] at H₃
+        rw [abs_sub_comm] at H₃
+        exact H₃.symm
+  · simp [State.dMove]; simp [State.ext_iff]
+    intro h₁ h₂ h₃ h₄ h₅
+    constructor
+    · rintro ⟨H₁, H₂⟩
+      constructor
+      · intro H₃ H₄
+        apply H₁; ext <;> assumption
+      · intro p₁ hp₁ hp₂ hp₃
+        apply H₂
+        convert hp₁
+        ext <;> simp_all
+    · rintro ⟨H₁, H₂⟩
+      constructor
+      · rintro rfl
+        simp at H₁
+      · intro H₃
+        specialize H₂ _ H₃
+        simp at H₂
+
+@[simp]
+instance : rotRight.WF where
+  initial_fs_iff := rotRight.cnd_initial_fs_iff
+  tr_eq := rotRight.cnd_tr_eq
+
+def rotLeft : sys.Symmetry :=
+  rotRight⁻¹
+
+@[simp] instance : rotLeft.WF := by unfold rotLeft; infer_instance
+
+def rot180 : sys.Symmetry :=
+  rotRight ^ 2
+
+@[simp] instance : rot180.WF := by unfold rot180; infer_instance
+
+@[simp] theorem rotRight_mul_rotRight : rotRight * rotRight = rot180 := rfl
+@[simp] theorem rotRight_mul_rotLeft : rotRight * rotLeft = 1 := by simp [rotLeft]
+@[simp] theorem rotLeft_mul_rotRight : rotLeft * rotRight = 1 := by simp [rotLeft]
+
+@[simp]
+theorem inv_rot180 : rot180⁻¹ = rot180 := by
+  rw [rot180, pow_two, mul_inv_rev, rotRight]
+  ext:2 <;> simp; ext:1 <;> simp
+
+@[simp]
+theorem rotLeft_mul_rotLeft : rotLeft * rotLeft = rot180 := by
+  rw [rotLeft, ←mul_inv_rev, ←pow_two, ←rot180]; simp
+
+@[simp] theorem rotRight_pow_two : rotRight ^ 2 = rot180 := rotRight_mul_rotRight
+@[simp] theorem rotLeft_pow_two : rotLeft ^ 2 = rot180 := rotLeft_mul_rotLeft
+
+@[simp]
+theorem rotRight_pow_three : rotRight ^ 3 = rotLeft := by
+  rw [pow_succ, rotRight_pow_two, ←rotLeft_mul_rotLeft, mul_assoc]; simp
+
+@[simp]
+theorem rotLeft_pow_three : rotLeft ^ 3 = rotRight := by
+  rw [pow_succ, rotLeft_pow_two, ←rotRight_mul_rotRight, mul_assoc]; simp
+
+@[simp]
+theorem rot180_mul_rot180 : rot180 * rot180 = 1 := by
+  nth_rw 1 [←inv_rot180]; simp [-inv_rot180]
+
+@[simp]
+theorem rot180_pow_two : rot180 ^ 2 = 1 :=
+  rot180_mul_rot180
+
+@[simp]
+theorem rotRight_pow_four : rotRight ^ 4 = 1 := by
+  rw [show 4 = 2 + 2 by rfl, pow_add]; simp
+
+@[simp]
+theorem rotLeft_pow_four : rotLeft ^ 4 = 1 := by
+  rw [show 4 = 2 + 2 by rfl, pow_add]; simp
+
+@[simp] theorem inv_rotRight : rotRight⁻¹ = rotLeft := rfl
+@[simp] theorem inv_rotLeft : rotLeft⁻¹ = rotRight := rfl
