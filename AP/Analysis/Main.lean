d49c497fcf0d1b1bee8dbf14e7a3dfa2de4ec982
Hakerh400 <hakerh403@gmail.com>
2025-09-20 16:52:31 +0200

    commit

--- AP/Analysis/Main.lean
+++ AP/Analysis/Main.lean
@@ -32,23 +32,6 @@ theorem tendsTo_unique {a L₁ L₂}
   simp at h₂
   linarith
 
-theorem tendsTo_one_div : tendsTo (1 / ·) 0 := by
-  intro e he
-  obtain ⟨N, hN⟩ := exists_nat_gt # 1 / e
-  use N
-  intro n hn
-  simp
-  rw [abs_of_nonneg # by simp]
-  have h₁ : 0 < (n : ℝ)
-  · cases n
-    · simp at hn; simp [hn] at hN; linarith
-    · simp; linarith
-  replace hn : (N : ℝ) ≤ n; exact_mod_cast hn
-  rw [inv_lt_iff_one_lt_mul₀ h₁]
-  replace hN : 1 < N * e
-  · rwa [←mul_inv_lt_iff₀ he]
-  nlinarith
-
 theorem tendsTo_drop_iff {a L k} : tendsTo (a # · + k) L ↔ tendsTo a L := by
   constructor
   · intro h e he
@@ -74,50 +57,51 @@ theorem tendsTo_drop_iff {a L k} : tendsTo (a # · + k) L ↔ tendsTo a L := by
 theorem converges_drop_iff {a k} : converges (a # · + k) ↔ converges a :=
   exists_congr # λ _ => tendsTo_drop_iff
 
--- #check 0 #exit
+theorem tendsTo_one_div : tendsTo (1 / ·) 0 := by
+  intro e he
+  obtain ⟨N, hN⟩ := exists_nat_gt # 1 / e
+  use N
+  intro n hn
+  simp
+  rw [abs_of_nonneg # by simp]
+  have h₁ : 0 < (n : ℝ)
+  · cases n
+    · simp at hn; simp [hn] at hN; linarith
+    · simp; linarith
+  replace hn : (N : ℝ) ≤ n; exact_mod_cast hn
+  rw [inv_lt_iff_one_lt_mul₀ h₁]
+  replace hN : 1 < N * e
+  · rwa [←mul_inv_lt_iff₀ he]
+  nlinarith
+
+theorem tendsTo_one_div_succ : tendsTo (λ n => 1 / (n + 1)) 0 := by
+  have h := tendsTo_drop_iff (a := (1 / ·)) (k := 1) (L := 0)
+  push_cast at h; rw [h]; exact tendsTo_one_div
 
 theorem not_converges_alternating {x y : ℝ} (h : x ≠ y) :
 ¬converges (if Even · then x else y) := by
-  sorry
-  -- -- wlog h₁ : x < y with ih
-  -- -- · apply ne_symm' at h
-  -- --   push_neg at h₁
-  -- --   specialize ih h # lt_of_le_of_ne h₁ h
-  -- --   contrapose! ih
-  -- --   rw [←converges_drop_iff (k := 1)]
-  -- --   simpa only [Nat.even_succ_iff, ←Nat.not_even_iff_odd, ite_not]
-  -- -- clear h
-  -- simp only [converges, tendsTo, not_exists, not_forall, not_lt]
-  -- intro L
-  -- generalize he₁ : |x - L| = e₁
-  -- generalize he₂ : |y - L| = e₂
-  -- use (e₁ + e₂) / 2
-  -- refine ⟨?_, ?_⟩
-  -- · subst he₁ he₂
-  --   simp
-  --   rw [LE.le.lt_iff_ne' # by positivity]
-  --   simp
-  --   rw [add_eq_zero_iff_of_nonneg (by simp) (by simp)]
-  --   simp
-  --   intro h₂ h₃
-  --   apply h
-  --   linarith
-  -- intro N
-  -- use N, by rfl
-  -- 
-  -- wlog h₁ : e₁ < e₂ with ih
-  -- · push_neg at h₁
-  --   apply ne_symm' at h
-  --   specialize ih h L e₂ he₂ e₁ he₁ N _
-  --   · rw [lt_iff_le_and_ne]; use h₁
-  --     rintro rfl
-  --     simp [←he₁, abs_eq_abs, h] at he₂
-  -- 
-  -- split_ifs with h₂
-  -- · rw [he₁]
+  simp only [converges, tendsTo, not_exists, not_forall, not_lt]
+  intro L
+  wlog h₁ : L ≠ x with ih
+  · apply ne_symm' at h
+    specialize ih h L _
+    · simp at h₁
+      subst h₁
+      exact ne_symm' h
+    obtain ⟨e, he, ih⟩ := ih
+    use e, he
+    intro N
+    specialize ih N
+    obtain ⟨n, hn, ih⟩ := ih
+    use n + 1, by linarith
+    simp_rw [Nat.even_succ_iff, ←Nat.not_even_iff_odd, ite_not]
+    exact ih
+  use |L - x|, by simp [sub_ne_zero_of_ne h₁]
+  intro N
+  use N * 2, by simp
+  simp
+  rw [abs_sub_comm]
 
--- #check 0 #exit
-
-theorem not_converges_one_minus_one : ¬converges ((-1 : ℝ) ^ ·) := by
+theorem not_converges_minus_one_pow : ¬converges ((-1 : ℝ) ^ ·) := by
   convert not_converges_alternating (x := 1) (y := -1) (by norm_num)
   nm n; induction n using Nat.mod_2_ind <;> simp
