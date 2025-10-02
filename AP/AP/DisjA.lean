ef1790da32d137486ddd1bcbb0c1d5f4671812b7
Hakerh400 <hakerh403@gmail.com>
2025-10-02 18:32:33 +0200

    commit

--- AP/AP/DisjA.lean
+++ AP/AP/DisjA.lean
@@ -11,29 +11,32 @@ def State.aForallWinsDisj (s : State) (fsp : FSP) (a : AStrat) : Prop :=
 def State.aHwsDisj (s : State) (fsp : FSP) : Prop :=
   ∃ (a : AStrat), a.WF ∧ s.aForallWinsDisj fsp a
 
-theorem AState.tr_of_aHwsDisj {s fsp} [hs : AState s] (h : s.aHwsDisj fsp) :
-∃ p s', sys.tr s p = some s' ∧ s'.aHwsDisj fsp.next := by
-  obtain ⟨a, Ha, h₁⟩ := h
+theorem AState.tr_of_aForallWinsDisj {s fsp} {a : AStrat}
+[hs : AState s] (h₁ : s.aForallWinsDisj fsp a) :
+∃ p s', sys.tr s p = some s' ∧ s'.aForallWinsDisj fsp.next a := by
   use a.f s
   obtain ⟨s', h₂⟩ : ∃ s', sys.tr s (a.f s) = some s'
-  · specialize h₁ default inferInstance
-    specialize h₁ 1
+  · specialize h₁ default inferInstance 1
     simp at h₁
     obtain ⟨s', h₁, -⟩ := h₁
     split at h₁; simp at h₁
     nm x s₁ h₂; use s₁
   use s', h₂
-  use a, Ha
   intro d Hd
   intro n
   specialize h₁ d Hd (n + 1)
   obtain ⟨s₁, h₁, h₃⟩ := h₁
   simp [h₂] at h₁; simp; use s₁
 
-theorem DState.tr_of_aHwsDisj {s s' p fsp} [hs : DState s] (h₁ : s.aHwsDisj fsp)
-(h₂ : sys.tr s p = some s') : s'.aHwsDisj fsp.next := by
-  obtain ⟨a, Ha, h₁⟩ := h₁
-  use a, Ha
+theorem AState.tr_of_aHwsDisj {s fsp} [hs : AState s] (h : s.aHwsDisj fsp) :
+∃ p s', sys.tr s p = some s' ∧ s'.aHwsDisj fsp.next := by
+  obtain ⟨a, ha, h⟩ := h
+  obtain ⟨p, s', h₁, h₂⟩ := hs.tr_of_aForallWinsDisj h
+  use p, s', h₁, a
+
+theorem DState.tr_of_aForallWinsDisj {s s' p fsp} {a : AStrat}
+[hs : DState s] [ha : a.WF] (h₁ : s.aForallWinsDisj fsp a)
+(h₂ : sys.tr s p = some s') : s'.aForallWinsDisj fsp.next a := by
   intro d Hd n
   specialize h₁ (d.set s p) (d.wf_set_of_tr h₂) (n + 1)
   obtain ⟨s₁, h₁, h₃⟩ := h₁
@@ -45,6 +48,11 @@ theorem DState.tr_of_aHwsDisj {s s' p fsp} [hs : DState s] (h₁ : s.aHwsDisj fs
   have hs' := sys.wf_of_tr h₂
   apply simulate_set_d_eq_of_length_hist_lt ⟨_, h₂⟩
   exact State.length_hist_lt_of_tr h₂
+
+theorem DState.tr_of_aHwsDisj {s s' p fsp} [hs : DState s] (h₁ : s.aHwsDisj fsp)
+(h₂ : sys.tr s p = some s') : s'.aHwsDisj fsp.next := by
+  obtain ⟨a, ha, h₁⟩ := h₁
+  use a, ha, tr_of_aForallWinsDisj h₁ h₂
 
 theorem State.aWinsDisj_iff {s : State} {fsp} {st : Strat} :
 s.aWinsDisj fsp st ↔ s.aWins st ∧ ∀ n, ¬fsp.hasLe n (sys.simulate st.f s n).1.aPos := by
@@ -125,43 +133,57 @@ s.aForallWinsDisj (fsp.insert 0 p) a := by
   rw [fsp.get_insert_of_ne # by simp] at h₁
   exact h₁
 
+theorem State.aHwsDisj_insert_of_mem_taken {s fsp p}
+[hs : sys.WF s] (h₁ : s.aHwsDisj fsp) (h₂ : p ∈ s.taken) :
+s.aHwsDisj (fsp.insert 0 p) := by
+  obtain ⟨a, ha, h₁⟩ := h₁; use a, ha, aForallWinsDisj_insert_of_mem_taken h₁ h₂
+
 -- #check 0 #exit
 
-theorem State.aForallWinsDisj_insert_of_not_mem_taken {s fsp p} {a : AStrat}
-[hs : sys.WF s] [ha : a.WF] (h₁ : s.aForallWinsDisj fsp a) (h₂ : p ∉ s.taken)
-(h₃ : p.dist s.aPos ≤ s.pw) : ∃ n, s.aForallWinsDisj (fsp.insert n p) a := by
+theorem AState.aHwsDisj_insert_of_not_mem_taken {s fsp p}
+[hs : AState s] (h₁ : s.aHwsDisj fsp) (h₂ : p ∉ s.taken)
+(h₃ : p.dist s.aPos ≤ s.pw) : ∃ n, s.aHwsDisj (fsp.insert n p) := by
   sorry
 
 -- #check 0 #exit
 
-theorem State.aForallWinsDisj_insert {s fsp} {p : PointZ} {a : AStrat}
-[hs : sys.WF s] [ha : a.WF] (h₁ : s.aForallWinsDisj fsp a) (h₂ : p.dist s.aPos ≤ s.pw) :
-∃ n, s.aForallWinsDisj (fsp.insert n p) a := by
-  by_cases h₃ : p ∈ s.taken
-  · exact ⟨0, s.aForallWinsDisj_insert_of_mem_taken h₁ h₃⟩
-  · exact aForallWinsDisj_insert_of_not_mem_taken h₁ h₃ h₂
+theorem State.aHwsDisj_insert_of_not_mem_taken {s fsp p}
+[hs : sys.WF s] (h₁ : s.aHwsDisj fsp) (h₂ : p ∉ s.taken)
+(h₃ : p.dist s.aPos ≤ s.pw) : ∃ n, s.aHwsDisj (fsp.insert n p) := by
+  replace hs := s.aState_or_dState
+  rcases hs with hs | hs
+  · exact hs.aHwsDisj_insert_of_not_mem_taken h₁ h₂ h₃
+  -- obtain ⟨p, s', h₄⟩ := hs.tr_of_aHwsDisj h₁
+  sorry
 
-theorem State.aForallWinsDisj_insertSet' {s fsp}  {ps : Set' PointZ} {a : AStrat}
-[hs : sys.WF s] [ha : a.WF] (h₁ : s.aForallWinsDisj fsp a)
-(h₂ : ∀ p ∈ ps, p.dist s.aPos ≤ s.pw) :
-∃ n, s.aForallWinsDisj (fsp.insertSet n ps.toSet) a := by
+#check 0 #exit
+
+theorem State.aHwsDisj_insert {s fsp} {p : PointZ}
+[hs : sys.WF s] (h₁ : s.aHwsDisj fsp) (h₂ : p.dist s.aPos ≤ s.pw) :
+∃ n, s.aHwsDisj (fsp.insert n p) := by
+  by_cases h₃ : p ∈ s.taken
+  · use 0; exact aHwsDisj_insert_of_mem_taken h₁ h₃
+  · exact aHwsDisj_insert_of_not_mem_taken h₁ h₃ h₂
+
+theorem State.aHwsDisj_insertSet' {s fsp} {ps : Set' PointZ}
+[hs : sys.WF s] (h₁ : s.aHwsDisj fsp) (h₂ : ∀ p ∈ ps, p.dist s.aPos ≤ s.pw) :
+∃ n, s.aHwsDisj (fsp.insertSet n ps.toSet) := by
   induction ps using Set'.ind; simpa; clear h₁
   nm ps p hp ih
   specialize ih _
   · simp_all only [Set'.mem_insert, or_true, implies_true, forall_const, forall_eq_or_imp]
   obtain ⟨n, ih⟩ := ih
-  obtain ⟨m, h₂⟩ := s.aForallWinsDisj_insert ih (p := p) #
+  obtain ⟨m, h₂⟩ := s.aHwsDisj_insert ih (p := p) #
     by simp_all only [Set'.mem_insert, forall_eq_or_imp]
   use max n m; simp
   rw [fsp.insertSet_set_insert]
-  apply aForallWinsDisj_insert_of_le _ # Nat.le_max_right n m
+  apply aHwsDisj_insert_of_le _ # Nat.le_max_right n m
   rw [fsp.insertSet_insert_comm] at h₂ ⊢
-  exact aForallWinsDisj_insertSet_of_le h₂ # Nat.le_max_left n m
+  exact aHwsDisj_insertSet_of_le h₂ # Nat.le_max_left n m
 
-theorem State.aForallWinsDisj_insertSet_of_finite {s fsp} {ps : Set PointZ} {a : AStrat}
-[hs : sys.WF s] [ha : a.WF] (h₁ : s.aForallWinsDisj fsp a) (h₂ : ps.Finite)
-(h₃ : ∀ p ∈ ps, p.dist s.aPos ≤ s.pw) :
-∃ n, s.aForallWinsDisj (fsp.insertSet n ps) a := by
-  have h₄ := s.aForallWinsDisj_insertSet' (ps := Set'.ofSet ps) h₁ #
+theorem State.aHwsDisj_insertSet_of_finite {s fsp} {ps : Set PointZ}
+[hs : sys.WF s] (h₁ : s.aHwsDisj fsp) (h₂ : ps.Finite)
+(h₃ : ∀ p ∈ ps, p.dist s.aPos ≤ s.pw) : ∃ n, s.aHwsDisj (fsp.insertSet n ps) := by
+  have h₄ := s.aHwsDisj_insertSet' (ps := Set'.ofSet ps) h₁ #
     by simpa [Set'.mem_ofSet h₂]
   rwa [Set'.toSet_ofSet h₂] at h₄
