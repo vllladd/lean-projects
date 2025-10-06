94e573dc1eb2bcb1d6d0670c3bad13bc7c53d4ee
Hakerh400 <hakerh403@gmail.com>
2025-10-06 20:28:37 +0200

    commit

--- AP/AP/Defense/Edge.lean
+++ AP/AP/Defense/Edge.lean
@@ -102,12 +102,399 @@ def defense (e : Edge) : Defense :=
 
 @[simp] theorem ps_defense : e.defense.ps = e.points := rfl
 
-set_option maxHeartbeats 1000000 in
 theorem of_eq_some {s p} (h : e.defense.f s = some p) :
 0 < e.dist s.aPos ∧ e.dist s.aPos ≤ 5 ∧ p ∉ s.taken ∧
 ∃ (z : ℤ), |z| ≤ 2 ∧ e.getBorderPoint s.aPos z = p := by
   simp [defense, f, f', getBorderPoints, getBorderPoint₀, List.find?_cons] at h
-  split at h <;> aesop'
+  split at h
+  -- all_goals aesop
+  · simp_all only [Option.some.injEq, Nat.ofNat_pos, le_refl, not_false_eq_true, true_and]
+    obtain ⟨left, right⟩ := h
+    obtain ⟨left_1, right⟩ := right
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      simp_all only [abs_zero, Nat.ofNat_nonneg]
+  · simp_all only [Int.reduceNeg, Nat.ofNat_pos, Int.reduceLE, not_false_eq_true, true_and]
+    obtain ⟨left, right⟩ := h
+    obtain ⟨left_1, right⟩ := right
+    split at left
+    rename_i x_1 heq_1
+    simp_all only [Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 4 => rename_i x_1 heq_1
+    on_goal 4 => split at left
+    on_goal 4 => rename_i x_2 heq_2
+    on_goal 5 => rename_i x_2 heq_2
+    on_goal 5 => split at left
+    on_goal 5 => rename_i x_3 heq_3
+    on_goal 6 => rename_i x_3 heq_3
+    on_goal 2 => {rfl}
+    · simp_all only [abs_zero, Nat.ofNat_nonneg]
+    simp_all only [Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq,
+      Int.reduceNeg, Bool.not_true, decide_eq_false_iff_not, Option.some.injEq,
+      not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_neg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq,
+      Int.reduceNeg, Bool.not_true, decide_eq_false_iff_not, Option.some.injEq,
+      not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq,
+      Int.reduceNeg, reduceCtorEq]
+  · simp_all only [Int.reduceNeg, Nat.ofNat_pos, Int.reduceLE, not_false_eq_true, true_and]
+    obtain ⟨left, right⟩ := h
+    obtain ⟨left_1, right⟩ := right
+    split at left
+    rename_i x_1 heq_1
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true,
+      decide_eq_false_iff_not, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 4 => rename_i x_1 heq_1
+    on_goal 4 => split at left
+    on_goal 4 => rename_i x_2 heq_2
+    on_goal 5 => rename_i x_2 heq_2
+    on_goal 5 => split at left
+    on_goal 5 => rename_i x_3 heq_3
+    on_goal 6 => rename_i x_3 heq_3
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, abs_neg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false,
+      decide_eq_true_eq, Bool.not_true, decide_eq_false_iff_not, Option.some.injEq,
+      not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false,
+      decide_eq_true_eq, Bool.not_true, decide_eq_false_iff_not, Option.some.injEq,
+      not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_zero, Nat.ofNat_nonneg]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false,
+      decide_eq_true_eq, reduceCtorEq]
+  · simp_all only [Int.reduceNeg, Nat.ofNat_pos, Int.reduceLE, not_false_eq_true, true_and]
+    obtain ⟨left, right⟩ := h
+    obtain ⟨left_1, right⟩ := right
+    split at left
+    rename_i h
+    split at left
+    rename_i x_1 heq_1
+    split at left
+    rename_i x_2 heq_2
+    simp_all only [Int.reduceNeg, decide_eq_true_eq, decide_true, Bool.not_true, Bool.false_and, Option.bind_some]
+    split at left
+    rename_i x_3 heq_3
+    split at left
+    rename_i x_4 heq_4
+    simp_all only [Int.reduceNeg, Bool.and_eq_true, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Option.elim_some, Option.some.injEq, not_false_eq_true, true_and]
+    subst left
+    obtain ⟨left, right_1⟩ := heq_4
+    apply Exists.intro
+    apply And.intro
+    on_goal 8 => rename_i h
+    on_goal 7 => rename_i x_1 heq_1
+    on_goal 6 => rename_i x_2 heq_2
+    on_goal 6 => split at left
+    on_goal 6 => rename_i x_3 heq_3
+    on_goal 7 => rename_i x_3 heq_3
+    on_goal 8 => split at left
+    on_goal 8 => rename_i x_2 heq_2
+    on_goal 9 => rename_i x_2 heq_2
+    on_goal 8 => split at left
+    on_goal 8 => rename_i x_3 heq_3
+    on_goal 9 => rename_i x_3 heq_3
+    on_goal 9 => split at left
+    on_goal 9 => rename_i x_4 heq_4
+    on_goal 10 => rename_i x_4 heq_4
+    on_goal 11 => split at left
+    on_goal 11 => rename_i x_3 heq_3
+    on_goal 12 => rename_i x_3 heq_3
+    on_goal 12 => split at left
+    on_goal 12 => rename_i x_4 heq_4
+    on_goal 13 => rename_i x_4 heq_4
+    on_goal 5 => rename_i x_3 heq_3
+    on_goal 4 => rename_i x_4 heq_4
+    on_goal 5 => split at left
+    on_goal 5 => rename_i x_4 heq_4
+    on_goal 6 => rename_i x_4 heq_4
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.and_eq_true, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Bool.and_eq_false_imp, Bool.not_false, decide_eq_true_eq, Option.elim_some, Option.some.injEq, not_false_eq_true,
+      true_and]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.and_eq_false_imp, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Bool.not_false, decide_eq_true_eq, Bool.and_eq_true, Option.elim_none, Option.some.injEq, not_false_eq_true,
+      true_and]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.and_eq_false_imp, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Bool.not_false, decide_eq_true_eq, Option.elim_none, reduceCtorEq]
+    simp_all only [Int.reduceNeg, decide_eq_true_eq, decide_eq_false_iff_not, decide_false, Bool.not_false,
+      Bool.true_and, decide_true, Bool.not_true, Bool.false_and, Option.bind_some]
+    split at left
+    rename_i x_4 heq_4
+    split at left
+    rename_i x_5 heq_5
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Bool.and_eq_true,
+      Option.elim_some, Option.some.injEq, not_false_eq_true]
+    subst left
+    obtain ⟨left, right_1⟩ := heq_5
+    apply Exists.intro
+    apply And.intro
+    on_goal 5 => rename_i x_4 heq_4
+    on_goal 4 => rename_i x_5 heq_5
+    on_goal 5 => split at left
+    on_goal 5 => rename_i x_5 heq_5
+    on_goal 6 => rename_i x_5 heq_5
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, abs_neg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Bool.and_eq_false_imp,
+      Bool.not_false, decide_eq_true_eq, Option.elim_some, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_neg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Bool.and_eq_true,
+      Bool.not_true, decide_eq_false_iff_not, Option.elim_none, Option.some.injEq, not_false_eq_true, true_and]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Bool.and_eq_false_imp,
+      Bool.not_true, decide_eq_false_iff_not, Option.elim_none, reduceCtorEq]
+    simp_all only [Int.reduceNeg, decide_eq_true_eq, decide_eq_false_iff_not, decide_false, Bool.not_false,
+      Bool.true_and, Option.bind_some, decide_true, Bool.not_true, Bool.false_and, Option.bind_none]
+    split at left
+    rename_i x_4 heq_4
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Option.elim_some,
+      Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 4 => rename_i x_4 heq_4
+    on_goal 4 => split at left
+    on_goal 4 => rename_i x_5 heq_5
+    on_goal 5 => rename_i x_5 heq_5
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, abs_neg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Bool.not_true,
+      decide_eq_false_iff_not, Option.elim_some, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Option.elim_none,
+      reduceCtorEq]
+    simp_all only [Int.reduceNeg, decide_eq_false_iff_not, decide_eq_true_eq, decide_true, Bool.not_true,
+      Bool.false_and, Option.bind_some, decide_false, Bool.not_false, Bool.true_and]
+    split at left
+    rename_i x_4 heq_4
+    split at left
+    rename_i x_5 heq_5
+    simp_all only [Int.reduceNeg, Bool.and_eq_true, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Option.elim_some, Option.some.injEq, not_false_eq_true, true_and]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 5 => rename_i x_4 heq_4
+    on_goal 4 => rename_i x_5 heq_5
+    on_goal 5 => split at left
+    on_goal 5 => rename_i x_5 heq_5
+    on_goal 6 => rename_i x_5 heq_5
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.and_eq_true, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Bool.not_false, decide_eq_true_eq, Option.elim_some, Option.some.injEq, not_false_eq_true, true_and]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.and_eq_false_imp, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Bool.not_false, decide_eq_true_eq, Option.elim_none, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_neg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.and_eq_false_imp, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not,
+      Bool.not_false, decide_eq_true_eq, Option.elim_none, reduceCtorEq]
+    simp_all only [Int.reduceNeg, decide_eq_false_iff_not, decide_eq_true_eq, decide_false, Bool.not_false,
+      Bool.true_and, decide_true, Bool.not_true, Bool.false_and, Option.bind_some]
+    split at left
+    rename_i x_5 heq_5
+    split at left
+    rename_i x_6 heq_6
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Option.elim_some,
+      Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 5 => rename_i x_5 heq_5
+    on_goal 4 => rename_i x_6 heq_6
+    on_goal 5 => split at left
+    on_goal 5 => rename_i x_6 heq_6
+    on_goal 6 => rename_i x_6 heq_6
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, abs_neg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Bool.not_false,
+      decide_eq_true_eq, Option.elim_some, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_neg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Bool.not_true,
+      decide_eq_false_iff_not, Option.elim_none, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_neg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Option.elim_none,
+      reduceCtorEq]
+    simp_all only [Int.reduceNeg, decide_eq_false_iff_not, decide_eq_true_eq, decide_false, Bool.not_false,
+      Bool.true_and, Option.bind_some, decide_true, Bool.not_true, Bool.false_and, Option.bind_none]
+    split at left
+    rename_i x_5 heq_5
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Option.elim_some,
+      Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 4 => rename_i x_5 heq_5
+    on_goal 4 => split at left
+    on_goal 4 => rename_i x_6 heq_6
+    on_goal 5 => rename_i x_6 heq_6
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, abs_neg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Bool.not_true,
+      decide_eq_false_iff_not, Option.elim_some, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, Nat.abs_ofNat, le_refl]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Option.elim_none,
+      reduceCtorEq]
+    simp_all only [Int.reduceNeg, decide_eq_false_iff_not, decide_eq_true_eq, decide_true, Bool.not_true,
+      Bool.false_and, Option.bind_none, decide_false, Bool.not_false, Bool.true_and, Option.bind_some, Option.elim_none]
+    split at left
+    rename_i x_4 heq_4
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Option.some.injEq,
+      not_false_eq_true]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 4 => rename_i x_4 heq_4
+    on_goal 4 => split at left
+    on_goal 4 => rename_i x_5 heq_5
+    on_goal 5 => rename_i x_5 heq_5
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, abs_neg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Bool.not_true,
+      decide_eq_false_iff_not, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, reduceCtorEq]
+    simp_all only [Int.reduceNeg, decide_eq_false_iff_not, decide_eq_true_eq, decide_false, Bool.not_false,
+      Bool.true_and, decide_true, Bool.not_true, Bool.false_and, Option.bind_none, Option.bind_some, Option.elim_none]
+    split at left
+    rename_i x_5 heq_5
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Option.some.injEq,
+      not_false_eq_true]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 4 => rename_i x_5 heq_5
+    on_goal 4 => split at left
+    on_goal 4 => rename_i x_6 heq_6
+    on_goal 5 => rename_i x_6 heq_6
+    on_goal 2 => {rfl}
+    · simp_all only [Int.reduceNeg, abs_neg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Bool.not_true,
+      decide_eq_false_iff_not, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Int.reduceNeg, Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, reduceCtorEq]
+    simp_all only [Int.reduceNeg, decide_eq_false_iff_not, decide_false, Bool.not_false, Bool.true_and,
+      Option.bind_none, Option.elim_none, reduceCtorEq]
+    simp_all only [Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [abs_zero, Nat.ofNat_nonneg]
+  · simp_all only [Int.reduceNeg, zero_lt_one, Nat.one_le_ofNat, not_false_eq_true, true_and]
+    obtain ⟨left, right⟩ := h
+    obtain ⟨left_1, right⟩ := right
+    split at left
+    rename_i x_1 heq_1
+    simp_all only [Bool.not_eq_eq_eq_not, Bool.not_true, decide_eq_false_iff_not, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    apply And.intro
+    on_goal 4 => rename_i x_1 heq_1
+    on_goal 4 => split at left
+    on_goal 4 => rename_i x_2 heq_2
+    on_goal 5 => rename_i x_2 heq_2
+    on_goal 5 => split at left
+    on_goal 5 => rename_i x_3 heq_3
+    on_goal 6 => rename_i x_3 heq_3
+    on_goal 2 => {rfl}
+    · simp_all only [abs_zero, Nat.ofNat_nonneg]
+    simp_all only [Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Int.reduceNeg, Bool.not_true,
+      decide_eq_false_iff_not, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_neg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Int.reduceNeg, Bool.not_true,
+      decide_eq_false_iff_not, Option.some.injEq, not_false_eq_true]
+    subst left
+    apply Exists.intro
+    · apply And.intro
+      on_goal 2 => {rfl}
+      · simp_all only [Int.reduceNeg, abs_one, Nat.one_le_ofNat]
+    simp_all only [Bool.not_eq_eq_eq_not, Bool.not_false, decide_eq_true_eq, Int.reduceNeg, reduceCtorEq]
+  · simp_all only [imp_false, reduceCtorEq, false_and]
 
 theorem dist_eq_zero_of_eq_some {s p} (h : e.defense.f s = some p) : e.dist p = 0 := by
   obtain ⟨-, h₁, h₂, z, h₃, rfl⟩ := of_eq_some h; simp [getBorderPoint]; split_ifs with h₄
@@ -128,7 +515,17 @@ theorem validTr_defense : e.defense.ValidTr := by
     simp [DState.validTr_iff]
     refine ⟨?_, h₃⟩
     simp [Point.ext_iff]
-    aesop'
+  -- all_goals aesop
+  · rintro rfl
+    simp_all only [abs_zero, Nat.ofNat_nonneg, add_zero]
+    apply Aesop.BuiltinRules.not_intro
+    intro a; simp_all only [lt_self_iff_false]
+  · intro a; simp_all only [lt_self_iff_false]
+  · intro a; simp_all only [lt_self_iff_false]
+  · rintro rfl
+    simp_all only [abs_zero, Nat.ofNat_nonneg, add_zero]
+    apply Aesop.BuiltinRules.not_intro
+    intro a; simp_all only [lt_self_iff_false]
 
 def cnd (e : Edge) (s : State) : Prop :=
   let pa := s.aPos
--- AP/Util/Meta.lean
+++ AP/Util/Meta.lean
@@ -9,6 +9,6 @@ macro_rules
 macro "nm " args:(ppSpace colGt Lean.binderIdent)+ : tactic =>
   `(tactic| rename_i $args*)
 
-axiom aesop' {P : Prop} : P
-
-macro "aesop'" : tactic => `(tactic| exact aesop')
+-- axiom aesop' {P : Prop} : P
+-- 
+-- macro "aesop'" : tactic => `(tactic| exact aesop')
