68137101c90d060bfcf40c0f6b1279180afdc836
Hakerh400 <hakerh403@gmail.com>
2025-10-29 18:02:16 +0100

    commit

--- AP/AP/Defense/Basic.lean
+++ AP/AP/Defense/Basic.lean
@@ -61,3 +61,116 @@ theorem sym_sym {sym₁ sym₂ : sys.Symmetry} : (dse.sym sym₁).sym sym₂ = d
     obtain ⟨left, right⟩ := h
     subst right
     simp_all only [System.Symmetry.ft'_ft]
+
+@[simp]
+theorem ps_sym {sym : sys.Symmetry} : (dse.sym sym).ps = sym.ft '' dse.ps := rfl
+
+-- #check 0 #exit
+
+theorem wf_sym_of_basicSym {sym} [H₁ : dse.WF] [H₂ : BasicSym sym] : dse.sym sym |>.WF := by
+  have H₁' := H₁
+  have wf := H₂.toWF
+  obtain ⟨ft, rfl⟩ := H₂.exi_mkSym; clear H₂
+  rcases H₁ with ⟨h₁, h₂⟩
+  constructor
+  · clear h₂
+    unfold ValidTr at h₁ ⊢
+    intro s hs p h₂
+    generalize mkSym ft = sym at wf h₂
+    have hs' : DState # sym.fs' s; simp
+    specialize @h₁ (sym.fs' s) _ (sym.ft' p) _
+    · simp [Defense.sym] at h₂ ⊢
+      obtain ⟨p, h₂, rfl⟩ := h₂
+      simpa
+    rw [sym⁻¹.validTr_iff]
+    simpa
+  · clear h₁
+    rintro s hs h₃ a Ha d Hd n
+    simp
+    
+    generalize ha' : AStrat.mk (λ s => (mkSym ft).ft' # a.f # (mkSym ft).fs s) = a'
+    generalize hd' : DStrat.mk (λ s => (mkSym ft).ft' # d.f # (mkSym ft).fs s) = d'
+    
+    have Ha' : a'.WF; subst ha'; infer_instance
+    have Hd' : d'.WF; subst hd'; infer_instance
+    
+    specialize @h₂ ((mkSym ft).fs' s) _ _ a' _ d' _ n
+    · simp [Defense.sym] at h₃; simpa
+    
+    generalize hsym : mkSym ft = sym at *
+    
+    rw [show ft.symm = sym.ft' by subst hsym; rfl]
+    
+    have h₄ : (dse.sym sym).st d |>.WF
+    · rw [DStrat.wf_iff]
+      intro sd hsd
+      simp [Defense.sym, st]
+      cases h₄ : dse.f # sym.fs' sd; simp
+      nm p
+      simp
+      replace h₄ := H₁'.1 h₄
+      rw [sym.validTr_iff']
+      simpa
+    
+    have h := @sys.simulate_congr_rel (f := Strat.f ⟨a', dse.st d'⟩)
+      (g := Strat.f ⟨a, dse.sym sym |>.st d⟩) (r := λ s' s => sym.fs s' = s)
+      (a₁ := sym.fs' s) (a₂ := s) (n := n) _ _ _ _ _ _ (by simp)
+    
+    specialize h _
+    · clear h h₂ h₃
+      
+      rintro k hk b₁ b₂ hb₁ hb₂ rfl
+      simp
+      intro c₁ c₂ hc₁ hc₂
+      
+      have Hb₁ := sys.wf_of_simulate_eq hb₁
+      have Hb₂ := sys.wf_of_simulate_eq hb₂
+      dsimp at Hb₁ Hb₂
+      
+      -- have ht : (sym.fs b₁).aTurn = b₁.aTurn
+      -- · subst hsym; simp
+      
+      replace Hb₁ := b₁.aState_or_dState
+      rcases Hb₁ with Hb₁ | Hb₁
+      
+      · replace Hb₂ : AState # sym.fs b₁
+        · use Hb₂; simp
+        
+        simp at hc₁ hc₂
+        replace hc₁ : sys.tr b₁ (sym.ft' # a.f # sym.fs b₁) = some c₁
+        · clear! d hb₁
+          subst ha'
+          simp at hc₁
+          
+          have h₁ : sys.validTr b₁ # sym.ft' # a.f # sym.fs b₁
+          · rw [sym.validTr_iff]
+            simp
+            use c₂
+          
+          simp [h₁] at hc₁
+          exact hc₁
+        
+        rw [sym.tr_eq] at hc₁
+        simp at hc₁
+        obtain ⟨c₂', hc₁, rfl⟩ := hc₁
+        simp [hc₁] at hc₂
+        simpa
+      
+      · replace Hb₂ : DState # sym.fs b₁
+        · use Hb₂; simp
+        
+        simp at hc₁ hc₂
+        
+        rw [sym.tr_eq'] at hc₂
+        simp at hc₂
+        obtain ⟨c₁', hc₂, rfl⟩ := hc₂
+        
+        simp [Defense.sym, st] at hc₁ hc₂
+        simp_rw [←Option.getD_map] at hc₂
+        simp at hc₂
+        
+        sorry
+    
+    obtain ⟨s', rfl, h⟩ := h
+    rw [h]; clear h
+    subst hsym; simpa
--- AP/AP/Defense/Edge/WF.lean
+++ AP/AP/Defense/Edge/WF.lean
@@ -27,35 +27,48 @@ theorem wf_defense_of_down (H : e.dir = .down) : e.defense.WF := by
 -- #check 0 #exit
 
 theorem wf_defense : e.defense.WF := by
-  use e.validTr_defense
-  intro s hs h a ha d hd n
-  cases H : e.dir
-  ·
-    -- obtain ⟨h₁, h₂⟩ := e.flipV.wf_defense_of_down (by simp [H])
-    -- have H₁ : Fact # e.flipV.dir = .down; simp [H]
-    -- specialize @h₂ (flipV.fs s) _ _ (a.sym flipV) _ (d.sym flipV) _ n
-    -- · simp [Edge.defense] at h ⊢; exact h
-    -- simp at h₂ ⊢
-    -- have h₃ := congrArg (·.sym rot180) e.flipV.defense_flipV_of_up
-    -- simp at h₃
-    -- rw [←h₃] at h₂; clear h₃
-    -- sorry
+  -- use e.validTr_defense
+  -- intro s hs h a ha d hd n
+  -- cases H : e.dir
+  -- ·
+  --   -- obtain ⟨h₁, h₂⟩ := e.flipV.wf_defense_of_down (by simp [H])
+  --   -- have H₁ : Fact # e.flipV.dir = .down; simp [H]
+  --   -- specialize @h₂ (flipV.fs s) _ _ (a.sym flipV) _ (d.sym flipV) _ n
+  --   -- · simp [Edge.defense] at h ⊢; exact h
+  --   -- simp at h₂ ⊢
+  --   -- have h₃ := congrArg (·.sym rot180) e.flipV.defense_flipV_of_up
+  --   -- simp at h₃
+  --   -- rw [←h₃] at h₂; clear h₃
+  --   -- sorry
+  --   sorry
+  -- ·
+  --   -- obtain ⟨h₁, h₂⟩ := e.rotRight.wf_defense_of_up (by simp [H])
+  --   -- have H₁ : Fact e.rotRight.hor; simp [H]
+  --   -- have h₃ := e.rotRight.defense_rotRight_of_hor
+  --   -- specialize @h₂ s _ _
+  --   sorry
+  -- ·
+  --   -- obtain ⟨h₁, h₂⟩ := e.rotRight.wf_defense_of_up (by simp [H])
+  --   -- have H₁ : Fact e.rotRight.hor; simp [H]
+  --   -- specialize @h₂ (rotRight.fs s) _ _ (a.sym rotRight) _ (d.sym rotRight) _ n
+  --   -- · simp [Edge.defense] at h ⊢; exact h
+  --   -- convert h₂ using 2
+  --   -- · symm
+  --   sorry
+  -- · apply wf_defense_of_down H |>.2 h
+  
+  cases h : e.dir
+  · have H : Fact # e.dir = .up; use h
+    have h₁ := congrArg (·.sym rot180) e.defense_flipV_of_up
+    simp at h₁
+    rw [←h₁]; clear h₁
+    have h₁ : e.flipV.defense.WF
+    · apply wf_defense_of_down; simp
     sorry
-  ·
-    -- obtain ⟨h₁, h₂⟩ := e.rotRight.wf_defense_of_up (by simp [H])
-    -- have H₁ : Fact e.rotRight.hor; simp [H]
-    -- have h₃ := e.rotRight.defense_rotRight_of_hor
-    -- specialize @h₂ s _ _
-    sorry
-  ·
-    -- obtain ⟨h₁, h₂⟩ := e.rotRight.wf_defense_of_up (by simp [H])
-    -- have H₁ : Fact e.rotRight.hor; simp [H]
-    -- specialize @h₂ (rotRight.fs s) _ _ (a.sym rotRight) _ (d.sym rotRight) _ n
-    -- · simp [Edge.defense] at h ⊢; exact h
-    -- convert h₂ using 2
-    -- · symm
-    sorry
-  · apply wf_defense_of_down H |>.2 h
+    
+  · sorry
+  · sorry
+  · exact wf_defense_of_down h
 
 -- #check 0 #exit
 
