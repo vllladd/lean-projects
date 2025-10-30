5cd32267d8b5da7b5fbe6a9bcc49bc51977735c2
Hakerh400 <hakerh403@gmail.com>
2025-10-30 17:21:18 +0100

    commit

--- AP/AP/Defense/Edge/Symmetry/Reflection.lean
+++ AP/AP/Defense/Edge/Symmetry/Reflection.lean
@@ -1,5 +1,20 @@
 import AP.AP.Defense.Edge.Symmetry.Basic
 
+namespace List
+
+variable {α β γ : Type*}
+variable {xs ys zs : List α}
+
+theorem find?_eq_some_equiv_iff {e : α ≃ α} {p : α → Bool} {x} :
+xs.find? p = some (e x) ↔ (xs.map e.symm).find? (p ∘ e) = some x := by
+  simp [find?_eq_some_iff_append]; constructor
+  · rintro ⟨h₁, xs, ⟨ys, rfl⟩, h₂⟩; use e x; simp [h₁]; use xs; simpa
+  · rintro ⟨x, ⟨h₁, xs, ⟨h₂, rfl⟩, h₃⟩, rfl⟩; simp [h₁]; use xs; simpa
+
+-- #check 0 #exit
+
+end List
+
 namespace AP.Edge
 
 variable {e e₁ e₂ : Edge}
@@ -77,21 +92,42 @@ theorem dist_rot180_ft_eq_dist_flipV_ft_of_hor [H : Fact e.hor] {p} :
 e.dist (rot180.ft p) = e.dist (-p) := by
   rcases e.dir_eq_or_eq_of_hor with h | h <;> simp [h, dist]
 
+-- @[simp]
+-- theorem map_flipV_ft_getBorderPoints_of_hor
+
 -- #check 0 #exit
 
 theorem defense_flipV_of_up [H : Fact # e.dir = .up] :
 e.flipV.defense = e.defense.sym rot180 := by
-  simp [defense, Defense.sym, dist, neg_sub_comm]
+  simp only [defense, dist, dir_flipV, hor, dir_eq_of_up, Dir.vert_up, ↓reduceIte, Dir.inv_up,
+    instFactTrue_aP, dir_eq_of_down, offset_flipV, points_flipV, Defense.sym,
+    System.Symmetry.SelfInverse.fs'_eq_fs, aPos_sym_of_basicSym, rot180_ft_y, neg_sub_comm,
+    rot180_points_eq_flipV_points_of_hor, Defense.mk.injEq, true_and]
   ext s p :2
-  simp [ft_eq_iff, f, f']
+  simp only [f, f', dist_flipV, System.Symmetry.SelfInverse.ft'_eq_ft, dist_flipV_ft_eq_of_hor,
+    getBorderPoint₀_flipV_of_hor, decide_not, getBorderPoints_flipV_of_hor, List.find?_append,
+    List.find?_map, Function.comp_def', List.find?_singleton, Bool.not_eq_eq_eq_not, Bool.not_true,
+    decide_eq_false_iff_not, ite_not, ne_eq, Option.pure_def, Option.bind_eq_bind,
+    Option.bind_eq_some_iff', Option.guard_eq_some', Option.some.injEq, exists_const, ↓existsAndEq,
+    and_true, aPos_sym_of_basicSym, dist_rot180_ft_eq_dist_flipV_ft_of_hor,
+    getBorderPoint₀_rot180_ft_of_hor, taken_sym_of_basicSym, Set'.mem_map, ft_eq_iff,
+    exists_eq_right, rot180_ft_flipH_ft, Option.map_bind, Function.comp_apply, Option.map_some,
+    EmbeddingLike.apply_eq_iff_eq, System.Symmetry.SelfInverse.ft_ft, and_congr_left_iff, and_imp]
   intro h₁ h₂
-  split
-  all_goals
-    nm x h₃; clear x
-    conv_rhs => rw [Equiv.option_eq_iff_map (e := rot180.ft)]
-  · simp
-  · sorry
-  · sorry
-  · sorry
-  · sorry
-  · simp
+  sorry
+  -- split <;> try simp [ft_eq_iff]
+  -- all_goals
+  --   nm x h₃; clear x
+  -- 
+  -- ·
+  --   iterate 2 rw [List.find?_eq_some_equiv_iff]
+  --   simp only [Function.comp_def', System.Symmetry.SelfInverse.ft_ft, System.Symmetry.ft_symm,
+  --     System.Symmetry.SelfInverse.ft'_eq_ft]
+  --     
+  --   suffices h : ((e.getBorderPoints s.aPos 1).map flipV.ft).find?
+  --     (λ x => !decide (x ∈ s.taken)) = ((e.getBorderPoints (rot180.ft s.aPos) 1).map
+  --     rot180.ft).find? (λ x => !decide (x ∈ s.taken))
+  --   · rw [h]
+  --   simp [getBorderPoints, getBorderPoint]
+  -- 
+  -- exact defense_flipV_of_up_fCase2
