b1c4ba0b3f3a3e78cb64b6de81bf95aaa0c206dd
Hakerh400 <hakerh403@gmail.com>
2025-11-10 16:44:39 +0100

    commit

--- AP/Inference/Basic.lean
+++ AP/Inference/Basic.lean
@@ -29,6 +29,9 @@ def ProofD.stat (p : ProofD) : Option Stat :=
       some Q
     | _, _ => none
 
+class ProofD.WF (p : ProofD) : Prop where
+  h : ∃ s, p.stat = some s
+
 structure Proof (e : Stat) : Prop where
   h : ∃ (p : ProofD), p.stat = some e
 
@@ -49,6 +52,7 @@ theorem mp {P Q} (h₁ : Proof # .imp P Q) (h₂ : Proof P) : Proof Q := by
 theorem mp' {P Q} (h₁ : Proof P) (h₂ : Proof # .imp P Q) : Proof Q :=
   mp h₂ h₁
 
+@[simp]
 theorem imp_refl : Proof # .imp (.var 0) (.var 0) := by
   generalize Stat.var 0 = P
   apply mp' # ax₁ P P
@@ -69,6 +73,27 @@ def ProofD.subst (p : ProofD) (i : ℕ) (s : Stat) : ProofD :=
   | .ax₂ P Q R => .ax₂ (P.subst i s) (Q.subst i s) (R.subst i s)
   | .ax₃ P Q => .ax₃ (P.subst i s) (Q.subst i s)
   | .mp p₁ p₂ => .mp (p₁.subst i s) (p₂.subst i s)
+
+theorem stat_mp_eq_some_iff {p₁ p₂ : ProofD} {s} : (p₁.mp p₂).stat = some s ↔
+∃ P, p₁.stat = some (.imp P s) ∧ p₂.stat = some P := by
+  simp
+  apply Iff.intro
+  · intro a
+    obtain ⟨w, h⟩ := a
+    obtain ⟨left, right⟩ := h
+    obtain ⟨w_1, h⟩ := right
+    obtain ⟨left_1, right⟩ := h
+    simp_all only [Option.some.injEq, exists_eq_right']
+    split at right
+    next __do_lift __do_lift_1 P Q =>
+      simp_all only [Option.bind_eq_some_iff', Option.guard_eq_some', Option.some.injEq,
+      exists_const]
+    next __do_lift __do_lift_1 x => simp_all only [imp_false, reduceCtorEq]
+  · intro a
+    obtain ⟨w, h⟩ := a
+    obtain ⟨left, right⟩ := h
+    simp_all only [Option.some.injEq, exists_eq_left', guard_true, Option.pure_def,
+      Option.bind_some]
 
 theorem ProofD.stat_subst_eq_some_of {s s' i} {p : ProofD}
 (h : p.stat = some s) : (p.subst i s').stat = some (s.subst i s') := by
@@ -119,9 +144,56 @@ theorem Proof.exi_shortest {s} {h : Proof s} : ∃ (p₁ : ProofD), p₁.stat = 
   rw [←h]
   exact ProofD.shortest_spec
 
--- @[simp]
--- theorem Proof.not_var {i} : ¬Proof (.var i) := by
---   intro h
---   replace h := h.exi_shortest
---   obtain ⟨p, h₁, h₂⟩ := h
---   sorry
+theorem Proof.exi_wf {s} {h : Proof s} : ∃ (p : ProofD), p.WF ∧ p.stat = some s := by
+  obtain ⟨p, h⟩ := h; exact ⟨p, ⟨s, h⟩, h⟩
+
+@[simp]
+def Stat.eval (s : Stat) (f : ℕ → Bool) : Bool := 
+  match s with
+  | .var i => f i
+  | .not a => !a.eval f
+  | .imp a b => !a.eval f ∨ b.eval f
+
+def Stat.Tauto (s : Stat) : Prop :=
+  ∀ f, s.eval f
+
+theorem tauto_of_proof {s} (h : Proof s) : s.Tauto := by
+  obtain ⟨p, h⟩ := h
+  intro f
+  induction p generalizing s
+  iterate 3 simp at h; subst h; simp; grind
+  nm p₁ p₂ ih₁ ih₂
+  rw [stat_mp_eq_some_iff] at h
+  choose s₁ h₁ h₂ using h
+  specialize ih₁ h₁
+  specialize ih₂ h₂
+  simp [ih₂] at ih₁
+  exact ih₁
+
+@[simp]
+theorem not_proof_var {i} : ¬Proof (.var i) := by
+  intro h
+  replace h := tauto_of_proof h
+  specialize h # λ _ => false
+  simp at h
+
+@[simp]
+theorem not_proof_not_var {i} : ¬Proof (.not # .var i) := by
+  intro h
+  replace h := tauto_of_proof h
+  specialize h # λ _ => true
+  simp at h
+
+def Stat.true : Stat :=
+  .imp (.var 0) (.var 0)
+
+def Stat.false : Stat :=
+  .not .true
+
+@[simp]
+theorem proof_true : Proof Stat.true :=
+  imp_refl
+
+@[simp]
+theorem tauto_true : Stat.true.Tauto := by
+  intro f; simp [Stat.true]
