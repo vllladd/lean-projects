65bf298ef41632b46442050f74f60cd0544b17bf
Hakerh400 <hakerh403@gmail.com>
2025-11-19 23:02:39 +0100

    commit

--- AP/RealAnalysis/Rearrangement.lean
+++ AP/RealAnalysis/Rearrangement.lean
@@ -63,11 +63,13 @@ theorem converges_rment_iff {a σ} (h₁ : Rment σ) :
 converges (a # σ ·) ↔ converges a := by
   apply exists_congr; simp [tendsTo_rment_iff h₁]
 
-theorem exi_lt_sum_of_monoLe {a L} (h₁ : ¬converges a) (h₂ : monoLe a) : ∃ N, L < a N := by
-  contrapose! h₁; apply converges_of_monoLe_and_bounded_top h₂ ⟨L, h₁⟩
+theorem exi_gt_of_monoLe_and_not_converges {a L}
+(h₁ : monoLe a) (h₂ : ¬converges a) : ∃ N, L < a N := by
+  contrapose! h₂; apply converges_of_monoLe_and_bounded_top h₁ ⟨L, h₂⟩
 
-theorem exi_lt_sum_of_monoGe {a L} (h₁ : ¬converges a) (h₂ : monoGe a) : ∃ N, a N < L := by
-  contrapose! h₁; apply converges_of_monoGe_and_bounded_bottom h₂ ⟨L, h₁⟩
+theorem exi_lt_of_monoGe_and_not_converges {a L}
+(h₁ : monoGe a) (h₂ : ¬converges a) : ∃ N, a N < L := by
+  contrapose! h₂; apply converges_of_monoGe_and_bounded_bottom h₁ ⟨L, h₂⟩
 
 def CondConv (a : ℕ → ℝ) : Prop :=
   converges (series a) ∧ ¬AbsConv a
@@ -152,13 +154,47 @@ theorem infp_of_drop {a p N} (h : Infp (a # N + ·) p) : Infp a p := by
 theorem infp_drop_iff {a p N} : Infp (a # N + ·) p ↔ Infp a p :=
    ⟨infp_of_drop, infp_drop_of⟩
 
--- #check 0 #exit
-
 theorem exi_mkSubseq_eq_of_apply {a p n} (h : Infp a p)
 (h₁ : p (a n)) : ∃ k, mkSubseq a p k = n := by
-  sorry
-
--- #check 0 #exit
+  induction n using Nat.strong_induction_on generalizing a
+  nm n ih
+  by_cases h₂ : ∀ k < n, ¬p (a k)
+  · use 0
+    rw [mkSubseq]
+    rw [Nat.findRaw_eq_iff, if_pos ⟨_, h₁⟩]
+    exact ⟨h₁, h₂⟩
+  push_neg at h₂
+  replace h₂ : ∃ k, k < n ∧ p (a k) ∧ ∀ r, k < r → r < n → ¬p (a r)
+  · choose k hk h₂ using h₂
+    obtain ⟨n, rfl⟩ := Nat.exists_eq_add_of_lt hk; clear hk
+    have h₃ : ∃ d, p # a # k + n - d
+    · use n
+      simpa
+    replace h₃ := Nat.findRaw_spec' h₃
+    generalize Nat.findRaw (λ d => p # a # k + n - d) = d at h₃
+    rcases h₃ with ⟨h₃, h₄⟩
+    use k + n - d, by omega, h₃
+    intro r h₅ h₆ h₇
+    rw [Nat.lt_succ_iff] at h₆
+    obtain ⟨w, h₈⟩ := Nat.exists_eq_add_of_le h₆
+    clear h₆
+    have h₆ : r = k + n - w; omega
+    rw [h₆] at h₇
+    specialize h₄ _ h₇
+    omega
+  choose k h₂ h₃ h₄ using h₂
+  specialize @ih k (by omega) a h h₃
+  choose i ih using ih
+  use i + 1
+  rw [mkSubseq, ih]; clear ih
+  obtain ⟨n, rfl⟩ := Nat.exists_eq_add_of_lt h₂; clear h₂
+  ring_nf at h₁ ⊢
+  simp
+  rw [Nat.findRaw_eq_iff]
+  rw [if_pos ⟨_, h₁⟩]
+  use h₁
+  intro w hw
+  apply h₄ <;> omega
 
 theorem apply_iff_exi_mkSubseq_eq {a p n} (h : Infp a p) : p (a n) ↔ ∃ k, mkSubseq a p k = n :=
   ⟨exi_mkSubseq_eq_of_apply h, λ ⟨_, h₁⟩ => apply_of_mkSubseq_eq h h₁⟩
@@ -166,8 +202,6 @@ theorem apply_iff_exi_mkSubseq_eq {a p n} (h : Infp a p) : p (a n) ↔ ∃ k, mk
 theorem exi_subseq_of_infp {a p} (h : Infp a p) :
 ∃ σ, Subseq σ ∧ ∀ n, p (a n) ↔ ∃ k, σ k = n :=
   ⟨_, subseq_mkSubseq, λ _ => apply_iff_exi_mkSubseq_eq h⟩
-
--- #check 0 #exit
 
 theorem infp_pos_of_condConv {a} (h : CondConv a) : Infp a (0 < ·) := by
   intro N
@@ -198,6 +232,20 @@ theorem infp_neg_of_condConv {a} (h : CondConv a) : Infp a (· < 0) := by
   have h₁ := infp_pos_of_condConv h N
   simp at h₁; exact h₁
 
+theorem monoLe_series_of_nonneg {a} (h : ∀ n, 0 ≤ a n) : monoLe (series a) := by
+  rw [monoLe_iff_le_succ]; intro n; rw [series_succ]; linarith [h n]
+
+theorem monoGe_series_of_nonpos {a} (h : ∀ n, a n ≤ 0) : monoGe (series a) := by
+  rw [monoGe_iff_succ_le]; intro n; rw [series_succ]; linarith [h n]
+
+theorem monoLt_series_of_pos {a} (h : ∀ n, 0 < a n) : monoLt (series a) := by
+  rw [monoLt_iff_lt_succ]; intro n; rw [series_succ]; linarith [h n]
+
+theorem monoGt_series_of_neg {a} (h : ∀ n, a n < 0) : monoGt (series a) := by
+  rw [monoGt_iff_succ_lt]; intro n; rw [series_succ]; linarith [h n]
+
+-- #check 0 #exit
+
 theorem exi_rment_tendsTo_of_condConv {a L} (h : CondConv a) :
 ∃ σ, Rment σ ∧ tendsTo (series (a # σ ·)) L := by
   -- suppose that series of `a` converges to `M`
@@ -206,10 +254,17 @@ theorem exi_rment_tendsTo_of_condConv {a L} (h : CondConv a) :
   have h₃ := infp_pos_of_condConv h
   -- infinitely many elements of `a` are negative
   have h₄ := infp_neg_of_condConv h
-  -- there exists a subsequence of `a` called `a+` that contains exactly positive elements of `a`
+  -- there exists a subsequence of `a` called `a+`
+  -- that contains exactly positive elements of `a`
+  obtain ⟨ap, hp₁, hp₂⟩ := exi_subseq_of_infp h₃
   -- similarly applies for `a-`
+  obtain ⟨an, hn₁, hn₂⟩ := exi_subseq_of_infp h₄
   -- series of `a+` is monotone
+  have h₅ : monoLe # series (a # ap ·)
+  · apply monoLe_series_of_nonneg; intro n; apply le_of_lt; rw [hp₂]; use n
   -- series of `a-` is antitone
+  have h₆ : monoGe # series (a # an ·)
+  · apply monoGe_series_of_nonpos; intro n; apply le_of_lt; rw [hn₂]; use n
   -- series of `a+` and series of `a-` cannot both converge
   --   suppose that series of `a+` converges to `X`
   --   suppose that series of `a-` converges to `-Y`
