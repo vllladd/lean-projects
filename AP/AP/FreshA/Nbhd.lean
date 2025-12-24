c1444f5890d6a1984ee4a4bbc8bd5dbfb0c36ecb
Hakerh400 <hakerh403@gmail.com>
2025-12-24 17:24:15 +0100

    commit

--- AP/AP/FreshA/Nbhd.lean
+++ AP/AP/FreshA/Nbhd.lean
@@ -20,50 +20,37 @@ theorem AState.aHwsDisj_of_tr {s s₁ p fsp} [hs : AState s] (h₁ : s₁.aHwsDi
   apply State.simulate_set_a_eq_of_length_hist_lt ⟨_, h₂⟩
   simp [length_hist_eq_of_tr h₂]
 
--- #check 0 #exit
-
 theorem AState.aHwsDisj_nbhd_pw {s : State} {fsp : FSP} [hs : AState s]
 (h : s.aHwsDisj fsp) : s.aHwsDisj # fsp.insertSet 3 # s.aPos.nbhd s.pw |>.toSet := by
   generalize hb : Set'.ofList (s.aPos.nbhd s.pw) = set
   replace h := exi_fresh1_of_aHwsDisj h
   choose a ha using h
-  
   have h : (s.aPos.nbhd s.pw).toSet = set.toSet
   · simp [←hb]
   rw [h]; clear h
-  
   have h₁ := State.aPtsSimNcard_spec_of_fresh1 ha set
-  
   generalize hf : (λ d => s.aPtsSimNcard ⟨a, d⟩ set.toSet) = f
   replace h₁ : ∀ (d : DStrat) [d.WF], ∃ n, f d = some n ∧ n ≤ set.size; grind
-  
   generalize hg : (λ d => f d |>.iget) = g
   have h₂ : ∀ (d : DStrat) [d.WF], g d ≤ set.size; grind
-  
   have h₃ : ∃ (d : DStrat), d.WF ∧ ∀ (d₁ : DStrat) [d₁.WF], g d₁ ≤ g d
-  ·
-    generalize set.size = N at h₂
+  · generalize set.size = N at h₂
     induction N
-    ·
-      simp at h₂
+    · simp at h₂
       use default, inferInstance
       intro d₁ hd₁
       simp [h₂]
     nm N ih
     by_cases h₃ : ∃ (d : DStrat), d.WF ∧ g d = N + 1
-    ·
-      choose d hd h₃ using h₃
+    · choose d hd h₃ using h₃
       use d, hd
       intro d₁ hd₁
       grind
     apply ih; grind
-  
   choose d hd h₃ using h₃
   clear h₂
-  
   replace h₁ : ∀ (d₁ : DStrat) [d₁.WF], ∃ n, n ≠ 0 ∧ f d₁ = some n ∧ n ≤ g d
-  ·
-    intro d₁ hd₁
+  · intro d₁ hd₁
     specialize h₁ d₁
     choose n h₁ h₄ using h₁
     use n; simp [h₁]
@@ -81,13 +68,10 @@ theorem AState.aHwsDisj_nbhd_pw {s : State} {fsp : FSP} [hs : AState s]
     rw [Point.dist_comm]
     rw [AState.tr_eq_some_iff] at h₅
     grind
-  
   rcases ha with ⟨ha, ha', -⟩
-  
   have h₄ : ∃ n sd, DState sd ∧ sys.simulate (Strat.f ⟨a, d⟩) s (n * 2 + 1) = (sd, 0) ∧
     sd.aPos ∈ set ∧ ∀ k s₁, 2 ≤ k → sys.simulate (Strat.f ⟨a, d⟩) sd k = (s₁, 0) → s₁.aPos ∉ set
-  ·
-    specialize h₁ d
+  · specialize h₁ d
     choose N h₁ h₄ h₅ using h₁
     clear h₅
     simp [←hf] at h₄
@@ -96,161 +80,101 @@ theorem AState.aHwsDisj_nbhd_pw {s : State} {fsp : FSP} [hs : AState s]
     choose n s₁ H₁ H₂ H₃ using h₄
     have hs₁ := DState.of_simulate_mul_two_add_one_eq_full H₁
     use n, s₁, hs₁, H₁, H₂, H₃
-  
   choose n sd hsd h₄ h₅ h₆ using h₄
-  
   have h₇ : sd.aHwsDisj # fsp.next.insertSet 2 set.toSet
-  ·
-    use a, ha
+  · use a, ha
     intro d₁ hd₁ k
-    
     generalize H₁ : DStrat.mk (λ s => some # if s.hist.length < sd.hist.length
       then d.f s else d₁.f s) = d₂
     have hd₂ : d₂.WF; rw [←H₁]; infer_instance
-    
     have H₂ : ∀ r, r ≤ n * 2 + 1 → sys.simulate (Strat.f ⟨a, d₂⟩) s r =
       sys.simulate (Strat.f ⟨a, d⟩) s r
-    ·
-      intro r hr
+    · intro r hr
       apply simulate_congr; simp
       intro r hr sd hsd H₂ H₃ H₄
       simp [←H₁]
       rw [if_pos]
       rotate_left
-      ·
-        rw [State.length_hist_eq_of_simulate_eq H₂]
+      · rw [State.length_hist_eq_of_simulate_eq H₂]
         rw [State.length_hist_eq_of_simulate_eq h₄]
         omega
       simp
-    
     have H₂' : sys.simulate (Strat.f ⟨a, d₂⟩) s (n * 2 + 1) = (sd, 0)
     · rw [←h₄]; apply H₂; rfl
-    
     have H₃ : sys.simulate (Strat.f ⟨a, d₂⟩) sd = sys.simulate (Strat.f ⟨a, d₁⟩) sd
-    ·
-      ext c :1
+    · ext c :1
       apply simulate_congr; simp
       intro r hr sd hsd H₃ H₄ H₅
       simp [←H₁]
       rw [if_neg]
       rotate_left
-      ·
-        push_neg
+      · push_neg
         apply length_hist_le_of_reachable
         exact sys.reachable_of_simulate_eq H₃
       simp
-    
     have ha₁ := ha' d₂ hd₂ (n * 2 + 1 + k)
     simp [H₂', H₃] at ha₁
-    
     choose s₁ H₄ H₅ using ha₁
     use s₁, H₄
-    
     replace H₅ : ¬fsp.next.hasLe k s₁.aPos
-    ·
-      contrapose! H₅
+    · contrapose! H₅
       rw [FSP.hasLe_next] at H₅
       apply FSP.hasLe_of_le H₅
       omega
-    
     simp [FSP.hasLe, FSP.insertSet] at H₅ ⊢
     intro r hr
     specialize H₅ r hr
     split_ifs with H₆
     on_goal 2 => exact H₅
     subst H₆
-    
     simp [H₅]; clear H₅
-    
-    -----
-    
     rename' hr => hk
-    
     specialize h₃ d₂
     contrapose! h₃
     simp [←hg, ←hf]
-    
     apply iget_aPtsSimNcard_lt_of (n * 2 + 1 + k) (State.aWins_of_aWinsDisj # ha' d hd)
       (State.aWins_of_aWinsDisj # ha' d₂ hd₂) (by grind) (by grind)
-    ·
-      intro r S₁ S₂ G₁ G₂ G₃
+    · intro r S₁ S₂ G₁ G₂ G₃
       simp at G₃ ⊢
-      
       by_cases hr : r ≤ n * 2 + 1
-      ·
-        grind
-      
+      · grind
       push_neg at hr
-      contrapose! G₃; clear G₃
+      contrapose! G₃
       obtain ⟨r, rfl⟩ := Nat.exists_eq_add_of_lt # le_of_lt hr
       replace hr : r ≠ 0; omega
-      
       by_cases hr' : r = 1
-      ·
-        clear hr
+      · clear hr
         subst hr'
-        clear! G₂
-        rw [sys.simulate_succ_full, ←H₂ _ # by rfl, H₂'] at G₁
-        simp at G₁
-        
-        -- specialize H₂ _ (by rfl)
-        -- rw [H₂'] at H₂
-        
-        sorry
-      
+        rw [sys.simulate_succ_full] at G₁ G₂
+        rw [←H₂ _ # by rfl] at G₁
+        simp [H₂'] at G₁ G₂
+        -- simp at G₁
+        rw [DState.aPos_eq_of_tr G₁]
+        rw [DState.aPos_eq_of_tr G₂] at G₃
+        exact G₃
       replace hr : 2 ≤ r
       · omega
       clear hr'
-      
+      clear G₃
       apply h₆ _ _ hr
-      
       rw [←Nat.add_one_add] at G₁
       rw [sys.simulate_add_full, ←H₂ _ # by rfl, H₂'] at G₁
       simp at G₁
       exact G₁
-    ·
-      intro S₃
+    · intro S₃
       rw [sys.simulate_add_full]
       specialize H₂ (n * 2 + 1) (by omega)
       rw [←H₂, H₂']
       simp
       intro G₁
       grind
-    ·
-      simpa [H₂, h₄, H₃, H₄]
-  
+    · simpa [H₂, h₄, H₃, H₄]
   generalize hk : n * 2 + 1 = k at h₄
-  
-  -- simp at h₄
-  -- choose sd h₄ H₁ using h₄
-  -- have hsd : sys.WF sd := sys.wf_of_simulate_eq h₄
-  -- replace hsd := DState.of_tr' H₁
-  -- simp at H₁
-  -- replace h₇ : sd.aHwsDisj # fsp.insertSet 2 set.toSet
-  -- ·
-  --   -- use a, ha
-  --   -- have H₂ := State.aForallWinsDisj_of_aForallWinsDisj_offset #
-  --   --   State.aForallWinsDisj_of_simulate_eq ha' h₄
-  --   -- intro d₁ hd₁ n
-  --   -- specialize H₂ d₁ hd₁ n
-  --   -- choose s₁ H₂ H₃ using H₂
-  --   -- use s₁, H₂
-  --   -- simp [FSP.hasLe, FSP.insertSet] at H₃ ⊢
-  --   -- intro r hr
-  --   -- split_ifs with H₄; on_goal 2 => grind
-  --   -- subst H₄
-  --   -- simp
-  --   -- symm; split_ands; grind
-  --   sorry
-  
   have H₂ := sys.reachable_of_simulate_eq h₄
   have H₃ := taken_subset_of_reachable H₂
-  
   replace h₆ : ∀ k, 2 ≤ k → ∃ s₁, sys.simulate (Strat.f ⟨a, d⟩) sd k = (s₁, 0) ∧ s₁.aPos ∉ set
-  ·
-    have h₆' : ∀ k, ∃ s₁, sys.simulate (Strat.f ⟨a, d⟩) sd k = (s₁, 0)
-    ·
-      intro r
+  · have h₆' : ∀ k, ∃ s₁, sys.simulate (Strat.f ⟨a, d⟩) sd k = (s₁, 0)
+    · intro r
       specialize ha' d hd (k + r)
       choose S h₈ h₉ using ha'
       simp [h₄] at h₈
@@ -260,13 +184,10 @@ theorem AState.aHwsDisj_nbhd_pw {s : State} {fsp : FSP} [hs : AState s]
     choose S h₆' using h₆'
     use S, h₆'
     exact h₆ r S hr h₆'
-  
   obtain ⟨sd₀, H₁⟩ : sys.validTr s sd.aPos
-  ·
-    simp [AState.validTr_iff]
+  · simp [AState.validTr_iff]
     split_ands
-    ·
-      specialize h₆ 2 (by omega)
+    · specialize h₆ 2 (by omega)
       choose S h₆ H₄ using h₆
       have hS := DState.of_simulate_mul_two_eq_full (n := 1) h₆
       simp at h₆
@@ -284,7 +205,6 @@ theorem AState.aHwsDisj_nbhd_pw {s : State} {fsp : FSP} [hs : AState s]
       exact H₅
     · apply Set'.not_mem_of_subset (s₂ := sd.taken) H₃; simp
     · simp [←hb] at h₅; rwa [Point.dist_comm]
-  
   have hsd₀ := DState.of_tr H₁
   replace h₇ : sd₀.aHwsDisj # fsp.next.insertSet 2 set.toSet
   · apply State.aHwsDisj_of_taken_subset' h₇
@@ -292,8 +212,6 @@ theorem AState.aHwsDisj_nbhd_pw {s : State} {fsp : FSP} [hs : AState s]
     · simp
     · rw [aPos_eq_of_tr H₁]
     · rwa [AState.taken_eq_of_tr H₁]
-  
   apply aHwsDisj_of_tr _ H₁
   · simp; exact State.aPos_not_mem_fsp_get_zero_of_aForallWinsDisj ha'
-  
   rwa [FSP.next_insertSet_succ]
