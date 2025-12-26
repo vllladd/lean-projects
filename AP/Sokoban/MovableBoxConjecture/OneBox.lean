607fd5564b6be486f8031260ad237cfab53eee4f
Hakerh400 <hakerh403@gmail.com>
2025-12-26 17:09:19 +0100

    commit

--- AP/Sokoban/MovableBoxConjecture/OneBox.lean
+++ AP/Sokoban/MovableBoxConjecture/OneBox.lean
@@ -18,15 +18,15 @@ open Classical in noncomputable
 def stateAM1Min : State :=
   Classical.epsilon # stateAM1Cnd sizeAM1Min
 
-def MovableBoxConjecture1 : Prop :=
-  ∃ (s : State), s.AlwaysMovable1
+class MovableBoxConjecture1 : Prop where
+  h : ∃ (s : State), s.AlwaysMovable1
 
 -- #check 0 #exit
 
 -----
 
 variable {s s₁ s₂ s₃ : State}
-variable (H : MovableBoxConjecture1)
+variable [H : MovableBoxConjecture1]
 include H
 
 omit H in
@@ -36,11 +36,38 @@ omit H in
 theorem State.AlwaysMovable1.wf (h : s.AlwaysMovable1) : sys.WF s := h.alwaysMovable.wf
 
 theorem exi_stateAM1Cnd : ∃ n s, stateAM1Cnd n s := by
-  choose s H using H; exact ⟨_, s, H, rfl⟩
+  choose s H using H.1; exact ⟨_, s, H, rfl⟩
 
 theorem sizeAM1Min_spec' : (∃ s, stateAM1Cnd sizeAM1Min s) ∧
 (∀ n, (∃ s, stateAM1Cnd n s) → sizeAM1Min ≤ n) :=
-  Nat.find!_spec' # exi_stateAM1Cnd H
+  Nat.find!_spec' exi_stateAM1Cnd
 
+theorem sizeAM1Min_spec : ∃ s, stateAM1Cnd sizeAM1Min s :=
+  sizeAM1Min_spec'.1
+
+theorem sizeAM1Min_le' {n} (h : ∃ s, stateAM1Cnd n s) : sizeAM1Min ≤ n :=
+  sizeAM1Min_spec'|>.2 n h
+
+theorem sizeAM1Min_le {n s} (h : stateAM1Cnd n s) : sizeAM1Min ≤ n :=
+  sizeAM1Min_le' ⟨_, h⟩
+
+@[simp]
 theorem stateAM1Min_spec : stateAM1Cnd sizeAM1Min stateAM1Min :=
-  Classical.epsilon_spec # sizeAM1Min_spec' H |>.1
+  Classical.epsilon_spec sizeAM1Min_spec
+
+@[simp]
+theorem alwaysMovable1_stateAM1Min : stateAM1Min.AlwaysMovable1 :=
+  stateAM1Min_spec.1
+
+@[simp]
+theorem alwaysMovable_stateAM1Min : stateAM1Min.AlwaysMovable :=
+  alwaysMovable1_stateAM1Min.alwaysMovable
+
+omit H in @[simp]
+theorem stateAM1Cnd_self : stateAM1Cnd s.boxesReachable.size s ↔ s.AlwaysMovable1 := by
+  simp [stateAM1Cnd]
+
+theorem stateAM1Min_size_boxesReachable_le {s} (h : stateAM1Cnd sizeAM1Min s) :
+s.boxesReachable.size ≤ stateAM1Min.boxesReachable.size := by
+  choose n h₁ using id h; rw [h₁]; apply sizeAM1Min_le'
+  use stateAM1Min; simp
